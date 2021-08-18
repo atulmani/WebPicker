e1be02c9c72e82d1b7614e4ecb154ffb4278c6f5
atulmani <atulmani@gmail.com>
2021-08-18 14:29:16 +0530

    Update main.js

--- ScreenCast/manu/t1/t1.1/js/main.js
+++ ScreenCast/manu/t1/t1.1/js/main.js
@@ -161,33 +161,30 @@ $(function() {
 // });
 
 $(document).ready(function() {
-  var owl = $('#banner-list');
-  owl.owlCarousel({
-    stagePadding: 30,
-    margin: 10,
-    nav: true,
-    loop: false,
-    autoplay: false,
-    // smartSpeed:3000,
-    // autoplayTimeout: 4000,
-    autoplayHoverPause: false,
-    autoWidth: false,
-    nav: false,
-    dots: false,
-    rtl: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 4
+    var owl = $('#banner-list');
+        owl.owlCarousel({
+      margin: 10,
+      loop: true,
+      autoplay: true,
+      smartSpeed:4000,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: false,
+      nav: false,
+      dots: false,
+      stagePadding: 50,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        }
       }
-    }
+    });
   });
-});
 
 $(document).ready(function() {
   var owl = $('#whc-owl-carousel');
