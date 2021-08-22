5d07955a6c6a08f7904a954b0acc8fb27b707260
atulmani <atulmani@gmail.com>
2021-08-22 17:14:40 +0530

    Create selecttemplate.js

---
+++ ScreenCast/manu/t1/t1.1/js/selecttemplate.js
@@ -0,0 +1,34 @@
+
+  $(document).ready(function(){
+    $('#1').click(function() {
+      hideall();
+      // $('.all').toggle("slide");
+      $('.text-ads').show("slide");
+    });
+    $('#2').click(function() {
+      hideall();
+      $('.first').show("slide");
+    });
+    $('#3').click(function() {
+      hideall();
+      $('.second').show("slide");
+    });
+    $('#4').click(function() {
+      hideall();
+      $('.third').show("slide");
+    });
+});
+
+function hideall(){
+  $('.text-ads').hide();
+  $('.first').hide();
+  $('.second').hide();
+  $('.third').hide();
+};
+
+// Menu's nav-link highlighted those are active - start
+  $(".select-tp-content-col .select-tp-card").on("click", function() {
+    $(".select-tp-content-col").find(".active").removeClass("active");
+    $(this).addClass("active");
+  });
+  // Menu's nav-link highlighted those are active - end
