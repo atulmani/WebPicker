4b3bf46e114aec84999006eea546ee488a700db6
atulmani <atulmani@gmail.com>
2021-08-23 11:08:22 +0530

    Create firebase-dashboard.js

---
+++ ScreenCast/manu/t1/t1.1/js/firebase-dashboard.js
@@ -0,0 +1,22 @@
+
+// ********** Firebase Services - Starts ************
+//A realtime Listerner
+auth.onAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    console.log(firebaseUser);
+    console.log("User has been logged in");
+  }else {
+    console.log('not logged in');
+    window.location.href = "../signup.html";
+  }
+});
+
+const btnLogout = document.getElementById ('btnLogout');
+
+btnLogout.addEventListener('click', e => {
+  auth.signOut();
+  window.location.href = "../signup.html";
+  console.log('Successfully Logged Out');
+});
+
+// ********** Firebase Services - Ends ************
