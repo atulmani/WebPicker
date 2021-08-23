73a35e8f4c4fe177f65a1c11d57bc7c4bbab3e78
atulmani <atulmani@gmail.com>
2021-08-23 11:08:48 +0530

    Create firebase.js

---
+++ ScreenCast/manu/t1/t1.1/js/firebase.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCawRJbDHaVLUGPNX5QcKO28o4V3NEFLkA",
+  projectId: "ads4-f3d81",
+  authDomain: "ads4-f3d81.firebaseapp.com",
+  // databaseURL: "https://webpicker-786c6-default-rtdb.firebaseio.com",
+  databaseURL: "https://ads4-f3d81.firebaseio.com",
+  storageBucket: "ads4-f3d81.appspot.com",
+  messagingSenderId: "247978042866",
+  appId: "1:247978042866:web:027c4a3a0ff7953bb9b76b",
+  // measurementId: "G-GJCEDPVJQL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
