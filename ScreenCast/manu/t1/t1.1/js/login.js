4a85eb283faf71e1db6f40df9e580fe5b71ca4b8
atulmani <atulmani@gmail.com>
2021-08-24 14:37:41 +0530

    Update login.js

--- ScreenCast/manu/t1/t1.1/js/login.js
+++ ScreenCast/manu/t1/t1.1/js/login.js
@@ -11,26 +11,26 @@ sign_in_btn.addEventListener('click', () => {
 });
 
 
-const toggleLoginEye = document.querySelector('#toggleLoginEye');
-const passwordSignin = document.querySelector('#txtPass_Signin');
-
-toggleLoginEye.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = passwordSignin.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordSignin.setAttribute('type', type);
-    // toggle the eye / eye slash icon
-    // this.classList.toggle('fa-eye');
-    toggleLoginEye.classList.toggle('fa-eye');
-});
-
-const toggleSignupEye = document.querySelector('#toggleSignupEye');
-const passwordSignup = document.querySelector('#txtPass_Signup');
-
-toggleSignupEye.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = passwordSignup.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordSignup.setAttribute('type', type);
-    // toggle the eye / eye slash icon
-    // this.classList.toggle('fa-eye');
-    toggleSignupEye.classList.toggle('fa-eye');
-});
+// const toggleLoginEye = document.querySelector('#toggleLoginEye');
+// const passwordSignin = document.querySelector('#txtPass_Signin');
+//
+// toggleLoginEye.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = passwordSignin.getAttribute('type') === 'password' ? 'text' : 'password';
+//     passwordSignin.setAttribute('type', type);
+//     // toggle the eye / eye slash icon
+//     // this.classList.toggle('fa-eye');
+//     toggleLoginEye.classList.toggle('fa-eye');
+// });
+//
+// const toggleSignupEye = document.querySelector('#toggleSignupEye');
+// const passwordSignup = document.querySelector('#txtPass_Signup');
+//
+// toggleSignupEye.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = passwordSignup.getAttribute('type') === 'password' ? 'text' : 'password';
+//     passwordSignup.setAttribute('type', type);
+//     // toggle the eye / eye slash icon
+//     // this.classList.toggle('fa-eye');
+//     toggleSignupEye.classList.toggle('fa-eye');
+// });
