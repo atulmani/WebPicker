68df85ea972a63a549128b79ec78d1246cde6185
atulmani <atulmani@gmail.com>
2021-08-27 18:10:59 +0530

    Update firebase-selecttemplate.js

--- ScreenCast/manu/t1/t1.1/js/firebase-selecttemplate.js
+++ ScreenCast/manu/t1/t1.1/js/firebase-selecttemplate.js
@@ -1,22 +1,35 @@
 
-// ********** Firebase Services - Starts ************
-//A realtime Listerner
-auth.onAuthStateChanged(firebaseUser => {
-  if(firebaseUser) {
-    console.log(firebaseUser);
-    console.log("User has been logged in");
-  }else {
-    console.log('not logged in');
-    window.location.href = "../signup.html";
-  }
+lnkLogout.addEventListener('click', e => {
+  auth.signOut().then(() => {
+    // Sign-out successful.
+    window.location.href = "../login";
+    console.log('Successfully Logged Out');
+  }).catch((error) => {
+    // An error happened.
+    console.log('Error: ' + error.message);
+    window.location.href = "../login";
+  });
 });
 
-const btnLogout = document.getElementById ('btnLogout');
-
-btnLogout.addEventListener('click', e => {
-  auth.signOut();
-  window.location.href = "../signup.html";
-  console.log('Successfully Logged Out');
-});
+//
+// // ********** Firebase Services - Starts ************
+// //A realtime Listerner
+// auth.onAuthStateChanged(firebaseUser => {
+//   if(firebaseUser) {
+//     console.log(firebaseUser);
+//     console.log("User has been logged in");
+//   }else {
+//     console.log('not logged in');
+//     window.location.href = "../signup.html";
+//   }
+// });
+//
+// const btnLogout = document.getElementById ('btnLogout');
+//
+// btnLogout.addEventListener('click', e => {
+//   auth.signOut();
+//   window.location.href = "../signup.html";
+//   console.log('Successfully Logged Out');
+// });
 
 // ********** Firebase Services - Ends ************
