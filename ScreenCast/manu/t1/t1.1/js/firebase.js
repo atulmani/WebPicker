ff8d4350cce95b3ec270998ee3d0b0f76a8eb367
atulmani <atulmani@gmail.com>
2021-09-12 12:26:25 +0530

    Update firebase.js

--- ScreenCast/manu/t1/t1.1/js/firebase.js
+++ ScreenCast/manu/t1/t1.1/js/firebase.js
@@ -1,16 +1,30 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// *********** Development Config **********
 var firebaseConfig = {
-  apiKey: "AIzaSyCawRJbDHaVLUGPNX5QcKO28o4V3NEFLkA",
-  projectId: "ads4-f3d81",
-  authDomain: "ads4-f3d81.firebaseapp.com",
-  // databaseURL: "https://webpicker-786c6-default-rtdb.firebaseio.com",
-  databaseURL: "https://ads4-f3d81.firebaseio.com",
-  storageBucket: "ads4-f3d81.appspot.com",
-  messagingSenderId: "247978042866",
-  appId: "1:247978042866:web:027c4a3a0ff7953bb9b76b",
+  apiKey: "AIzaSyB7NY05st0jH8nWmUuNmQHYJGtmp9Pe0M0",
+  projectId: "hyperclouddev",
+  authDomain: "hyperclouddev.firebaseapp.com",
+  databaseURL: "https://hyperclouddev.firebaseio.com",
+  storageBucket: "hyperclouddev.appspot.com",
+  messagingSenderId: "939801651782",
+  appId: "1:939801651782:web:f63ad3e0469b27838c7847",
   // measurementId: "G-GJCEDPVJQL"
 };
+
+// *********** Production Config **********
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAbSjrm6vOca3CgLU0qAEtn9tLSwSqNdk0",
+//   projectId: "hypercloudprod",
+//   authDomain: "hypercloudprod.firebaseapp.com",
+//   databaseURL: "https://hypercloudprod.firebaseio.com",
+//   storageBucket: "hypercloudprod.appspot.com",
+//   messagingSenderId: "373925918977",
+//   appId: "1:373925918977:web:99a6729a8eff497faa8300",
+//   measurementId: "G-RRM25JW0R6"
+// };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
