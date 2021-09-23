de47797d7708f5997b9f291f04397f544bfa3424
Sanidhya-ctrl <72963786+Sanidhya-ctrl@users.noreply.github.com>
2021-09-23 11:53:17 +0530

    Update fb-all-events.js

--- Agro/dolu/t1/t1.1/js/fb-all-events.js
+++ Agro/dolu/t1/t1.1/js/fb-all-events.js
@@ -1,4 +1,3 @@
-
 const profileLogo = document.getElementById('profileLogo');
 const profileLogoDropdown = document.getElementById('profileLogoDropdown');
 const profileLogoTriangle = document.getElementById('profileLogoTriangle');
@@ -8,8 +7,7 @@ const fullContent = document.getElementById('fullContent');
 
 profileLogo.addEventListener('click', profileDropdownShow, false);
 
-function profileDropdownShow()
-{
+function profileDropdownShow() {
   profileLogoDropdown.style.visibility = "visible";
   profileLogoTriangle.style.visibility = "visible";
 }
@@ -17,8 +15,7 @@ function profileDropdownShow()
 fullContent.addEventListener('click', profileDropdownHide, false);
 // navbar.addEventListener('click', profileDropdownHide, false);
 
-function profileDropdownHide()
-{
+function profileDropdownHide() {
   profileLogoDropdown.style.visibility = "hidden";
   profileLogoTriangle.style.visibility = "hidden";
 }
@@ -40,7 +37,7 @@ auth.onAuthStateChanged(firebaseUser => {
 
     } else {
       // console.log('User has been logged out');
-       window.location.href = "index.html";
+      window.location.href = "index.html";
     }
   } catch (error) {
     console.log(error.message);
@@ -50,14 +47,13 @@ auth.onAuthStateChanged(firebaseUser => {
 
 
 
-function GetProfileData (user)
-{
+function GetProfileData(user) {
   // const ref = db.collection("Users").doc(user.uid);
 
   const snapshot = db.collection('Users').doc(user.uid);
-  snapshot.get().then( async ( doc ) => {
-    if ( doc.exists ) {
-      // let blogPost = doc.data();
+  snapshot.get().then(async (doc) => {
+      if (doc.exists) {
+        // let blogPost = doc.data();
         // console.log ('User UID: ' + user.uid);
         // console.log ('Document ref id: ' + doc.data().uid);
         // console.log('Display Name: '+ doc.data().displayName);
@@ -88,12 +84,118 @@ function GetProfileData (user)
         // document.getElementById('idno').value = doc.data().IDNo;
         // document.getElementById('address').value = doc.data().Address;
 
-    }
-  })
-  .catch((error) => {
-    // An error occurred
-    console.log(error.message);
-    // document.getElementById('errorMessage_Signup').innerHTML = error.message;
-    // document.getElementById('errorMessage_Signup').style.display = 'block';
-  });
+      }
+    })
+    .catch((error) => {
+      // An error occurred
+      console.log(error.message);
+      // document.getElementById('errorMessage_Signup').innerHTML = error.message;
+      // document.getElementById('errorMessage_Signup').style.display = 'block';
+    });
 };
+
+//**************************SELECT/GET DATA*****************************//
+
+db.collection("Events").onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  // alert('Snapsize from Homepage: ' + snapshot.size);
+  // console.log(changes);
+  changes.forEach(change => {
+    if (change.type == 'added') {
+      renderEvents(change.doc);
+    }
+  });
+});
+
+
+function renderEvents(doc) {
+  console.log('Doc ID: ' + doc.id);
+  console.log('Event Name: ' + doc.data().EventName);
+
+  let itemCol = document.createElement("div");
+  // itemCol.classList.add('col-lg-3 col-md-6 col-sm-12');
+  itemCol.classList.add("col-lg-3");
+  itemCol.classList.add("col-md-6");
+  itemCol.classList.add("col-sm-12");
+  itemCol.innerHTML = "<a style='text-decoration:none;' href='createEvent.html?id=" + doc.id + "&eventid=" + doc.data().EventId + "'>" +
+    "<div class='item'>" +
+    "<div class='post-slide'>" +
+    "<div class='post-img'>" +
+    "<img style='border-radius:10px;' src='" + doc.data().EventImgURL + "' alt='' width='100%'>" +
+    "</div>" +
+    "<div class='post-content'>" +
+    "<h3 class='post-title'>" +
+    doc.data().EventName + "</h3>" +
+    "<p class='description'>" +
+      "<span style='letter-spacing:1px;font-weight:1000;'><span style='font-size:15px;'>By: </span>" +
+       doc.data().EventOrganisationName +
+       "</span>" +
+       "<br>" +
+       "<span style='letter-spacing:1px;font-weight:1000;line-height:30px;'><span style='font-weight:1000;'>Date: </span>" + doc.data().EventStartDate + " - " + doc.data().EventEndDate +
+        "</span>" +
+        "<br>" +
+        "<span style='letter-spacing:1px;font-weight:1000;line-height:14px;'><span style='font-weight:1000'>Price: ₹</span>" + doc.data().Price + "</span>" +
+         "<br>" +
+    "</p>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</a>";
+
+
+
+
+
+
+
+
+  document.getElementById("eventRow").appendChild(itemCol);
+
+
+
+
+  // itemlist = document.getElementById("firebase-event-list");
+  // let item = document.createElement('div');
+  // item.classList.add("item");
+  // // item.innerHTML = "<div class='post-content'><h3 class='post-title'>" + change.doc.data().EventName + "</h3></div>";
+  //
+  //
+  // var link = "<a style='text-decoration:none;' href='createEvent.html?id=" + change.doc.data().EventId + "'>";
+  //
+  //
+  // // item.innerHTML = "<a style='text-decoration:none;' href='./Events/EventDetails.html?id=" + change.doc.data().EventId + ">" +
+  // item.innerHTML = link +
+  //   "<div class='item'>" +
+  //     "<div class='post-slide'>" +
+  //       "<div class='post-img'>" +
+  //           "<img style='border-radius:10px;' src='" + change.doc.data().EventImgURL + "' alt='' width='100%'>" +
+  //         "</div>" +
+  //         "<div class='post-content'>" +
+  //             "<h3 class='post-title' id='homepage_eventname'>" + change.doc.data().EventName +
+  //             "</h3>" +
+  //             "<p class='description'>" +
+  //                 "<span style='font-weight:bold'>Venue: </span>" + change.doc.data().EventVenue +
+  //                 "<br>" +
+  //                 "<span style='letter-spacing:1px;' id='homepage_eventvenue'></span>" +
+  //                 "<br>" +
+  //                 "<span style='font-weight:bold'>Date: </span>" + formatDate(change.doc.data().EventStartDate) + ' to ' + formatDate(change.doc.data().EventEndDate) +
+  //                 "<br>"
+  //                 "<span style='letter-spacing:1px;line-height:30px;'  id='homepage_eventdate'>" +
+  //                 "</span>" +
+  //                 "<br>" +
+  //                 "<span style='letter-spacing:1px;line-height:14px;'><span style='font-weight:bold'>Price</span>: ₹ 4999 </span>" +
+  //               "</p>" +
+  //             "</div>" +
+  //           "</div>" +
+  //         "</div>" +
+  //   "</a>"
+
+
+  // $('#event-list').trigger('add.owl.carousel', [item]).trigger('refresh.owl.carousel');
+
+  // console.log(change.doc.data().EventName);
+
+
+
+
+}
