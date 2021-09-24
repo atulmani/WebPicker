42a4679c84b06c0e47ec849ca826f9242d865016
atulmani <atulmani@gmail.com>
2021-09-24 14:19:49 +0530

    Update firebase-admin-common.js

--- ScreenCast/manu/t1/t1.2/js/firebase-admin-common.js
+++ ScreenCast/manu/t1/t1.2/js/firebase-admin-common.js
@@ -1,23 +1,4 @@
 // ********** Firebase Services - Starts ************
-//A realtime Listerner
-auth.onAuthStateChanged(firebaseUser => {
-  try {
-    if (firebaseUser) {
-      // console.log(firebaseUser);
-      console.log('User: ' + firebaseUser.email + ' is logged-In');
-      // console.log("UID: " + firebaseUser.uid);
-      // console.log("Display Name: " + firebaseUser.displayName);
-      // console.log("Email ID: " + firebaseUser.email);
-      // document.getElementById('displayName').innerHTML = firebaseUser.displayName;
-    } else {
-      console.log('User has been logged out');
-      window.location.href = "../login";
-    }
-  } catch (error) {
-    console.log(error.message);
-    window.location.href = "../login";
-  }
-});
 
 const btnLogout = document.getElementById ('btnLogout');
 
