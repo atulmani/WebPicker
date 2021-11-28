83265d4e7e0722aa876fe6f383bffe4d5a9a2406
atulmani <atulmani@gmail.com>
2021-11-28 19:22:05 +0530

    Update deliveryOptionNew.js

--- Agrikaash/manu/t1/t1.1/js/deliveryOptionNew.js
+++ Agrikaash/manu/t1/t1.1/js/deliveryOptionNew.js
@@ -117,8 +117,171 @@ function GetDeliveryAddress() {
   });
 
 }
+var cartDetails = [];
+var addressList = [];
+var selectedAddress;
+var orderDetails = [];
+var message = "";
+var iError = 0;
+
+async function getCartDetails() {
+  const snapshot = db.collection('CartDetails').doc(userID);
+  snapshot.get().then(async (doc) => {
+    if (doc.exists) {
+      console.log('get cart details');
+      cartDetails = doc.data().cartDetails;
+    } else {
+      message = "cart is empty, add to cart";
+      iError = 1;
+      console.log("cart is empty, add to cart");
+    }
+  });
+  return message;
+}
+
+async function getAddress() {
+  const snapshotAddress = db.collection('AddressList').doc(userID);
+  snapshotAddress.get().then(async (adoc) => {
+    if (adoc.exists) {
+      console.log('if address exists');
+      addressList = adoc.data().AddressList;
+      var selIndex = -1;
+      selIndex = addressList.findIndex(e => e.addressSelected === 'YES');
+      if (selIndex >= 0) {
+        selectedAddress = addressList[selIndex];
+        console.log('if address selected');
+      } else {
+        message = message + "Select Address";
+        iError = 2;
+      }
+    } else {
+      message = message + "Add Address";
+      iError = 3;
+
+      console.log("Add Address");
+    }
+  });
+  return message;
+}
 
 function SaveOrder() {
+
+  var orderDetails = [];
+  var message = "";
+  var iError = 0;
+  console.log('SaveOrder');
+  getCartDetails()
+    .then(async (message) => {
+      getAddress()
+      .then(async (message) =>{
+
+          var deliveryDate = '';
+
+          if (document.getElementById("delDate1").checked) {
+            deliveryDate = document.getElementById("delDate1").value;
+          } else if (document.getElementById("delDate2").checked) {
+            deliveryDate = document.getElementById("delDate2").value;
+          } else if (document.getElementById("delDate3").checked) {
+            deliveryDate = document.getElementById("delDate3").value;
+          } else if (document.getElementById("delDate4").checked) {
+            deliveryDate = document.getElementById("delDate4").value;
+          } else if (document.getElementById("delDate5").checked) {
+            deliveryDate = document.getElementById("delDate5").value;
+          } else if (document.getElementById("delDate6").checked) {
+            deliveryDate = document.getElementById("delDate6").value;
+          } else if (document.getElementById("delDate7").checked) {
+            deliveryDate = document.getElementById("delDate7").value;
+          }
+          var deliveryTime = '';
+          if (document.getElementById("delTime1").checked) {
+            deliveryTime = document.getElementById("delTime1").value;
+          } else if (document.getElementById("delTime2").checked) {
+            deliveryTime = document.getElementById("delTime2").value;
+          } else if (document.getElementById("delTime3").checked) {
+            deliveryTime = document.getElementById("delTime3").value;
+          } else if (document.getElementById("delTime4").checked) {
+            deliveryTime = document.getElementById("delTime4").value;
+          }
+
+          var paymentOption = "";
+          if (document.getElementById("PayOption1").checked) {
+            paymentOption = document.getElementById("PayOption1").value;
+          } else if (document.getElementById("PayOption2").checked) {
+            paymentOption = document.getElementById("PayOption2").value;
+          } else if (document.getElementById("PayOption3").checked) {
+            paymentOption = document.getElementById("PayOption3").value;
+          }
+
+          var prize = document.getElementById("totalAmount").innerHTML;
+          var itemCount = document.getElementById("itemCount").innerHTML;
+          const snapshotOrder = db.collection('OrderDetails').doc(userID);
+          console.log("before insert");
+          snapshotOrder.get().then(async (aOrder) => {
+            if (aOrder.exists) {
+              console.log('if order exists');
+              orderDetails = aOrder.data().OrderDetails;
+            }
+            orderDetails.push({
+              orderID: 'Ord-' + userID + Date.now(),
+              orderItems: cartDetails,
+              totalItems: itemCount,
+              totalAmount: prize,
+              deliveryAddress: selectedAddress,
+              deliveryDate: deliveryDate,
+              deliveryTime: deliveryTime,
+              paymentOption: paymentOption,
+              paymentStatus: 'Pending',
+              orderStatus: 'Pending',
+              orderDate: (new Date()).toString(),
+              orderBy: userID
+
+            });
+
+            console.log('insert order');
+            db.collection('OrderDetails')
+              .doc(userID)
+              .set({
+                OrderDetails: orderDetails,
+                CreatedBy: auth.currentUser.email,
+                CreatedTimestamp: (new Date()).toString(),
+                UpdatedBy: '',
+                UpdatedTimestamp: ''
+              })
+              .then((docRef) => {
+                console.log("Data added sucessfully in the document: ");
+                //showAddress(false);
+                //delete from cart after order places
+                cartDetails = [];
+                db.collection('CartDetails')
+                  .doc(userID)
+                  .update({
+                    cartDetails: cartDetails
+                  })
+                  .then((docred) => {
+                    console.log('cart details made blank');
+
+                  });
+
+                // console.log(Date.parse(eventstart))
+              })
+              .catch((error) => {
+                console.log(error);
+                //  console.error("error adding document:", error.message);
+              });
+
+          });
+      });
+      console.log("after address");
+    })
+    .finally()
+    {
+      console.log("in finally");
+    };
+    window.location.href = "orderList.html";
+
+}
+/*
+function SaveOrder1() {
   var cartDetails = [];
   var addressList = [];
   var selectedAddress;
@@ -133,136 +296,85 @@ function SaveOrder() {
 
     const snapshotAddress = db.collection('AddressList').doc(userID);
     snapshotAddress.get().then(async (adoc) => {
-      if (adoc.exists) {
-        console.log('if address exists');
-        addressList = adoc.data().AddressList;
-        var selIndex = -1;
-        selIndex = addressList.findIndex(e => e.addressSelected === 'YES');
-        if (selIndex >= 0) {
-          selectedAddress = addressList[selIndex];
-          console.log('if address selected');
-          //save orders
-          const snapshotOrder = db.collection('OrderDetails').doc(userID);
-          snapshotOrder.get().then(async (aOrder) => {
-            if (aOrder.exists) {
-              console.log('if order exists');
-              orderDetails = aOrder.data().OrderDetails;
-              //get delivery timeout
-              var deliveryDate = '';
+        if (adoc.exists) {
+          console.log('if address exists');
+          addressList = adoc.data().AddressList;
+          var selIndex = -1;
+          selIndex = addressList.findIndex(e => e.addressSelected === 'YES');
+          if (selIndex >= 0) {
+            selectedAddress = addressList[selIndex];
+            console.log('if address selected');
+            //save orders
+            //get delivery timeout
+            var deliveryDate = '';
 
-              if (document.getElementById("delDate1").checked) {
-                deliveryDate = document.getElementById("delDate1").value;
-              } else if (document.getElementById("delDate2").checked) {
-                deliveryDate = document.getElementById("delDate2").value;
-              } else if (document.getElementById("delDate3").checked) {
-                deliveryDate = document.getElementById("delDate3").value;
-              } else if (document.getElementById("delDate4").checked) {
-                deliveryDate = document.getElementById("delDate4").value;
-              } else if (document.getElementById("delDate5").checked) {
-                deliveryDate = document.getElementById("delDate5").value;
-              } else if (document.getElementById("delDate6").checked) {
-                deliveryDate = document.getElementById("delDate6").value;
-              } else if (document.getElementById("delDate7").checked) {
-                deliveryDate = document.getElementById("delDate7").value;
-              }
-              var deliveryTime = '';
-              if (document.getElementById("delTime1").checked) {
-                deliveryTime = document.getElementById("delTime1").value;
-              } else if (document.getElementById("delTime2").checked) {
-                deliveryTime = document.getElementById("delTime2").value;
-              } else if (document.getElementById("delTime3").checked) {
-                deliveryTime = document.getElementById("delTime3").value;
-              } else if (document.getElementById("delTime4").checked) {
-                deliveryTime = document.getElementById("delTime4").value;
-              }
-
-              var paymentOption = "";
-              if (document.getElementById("PayOption1").checked) {
-                paymentOption = document.getElementById("PayOption1").value;
-              } else if (document.getElementById("PayOption2").checked) {
-                paymentOption = document.getElementById("PayOption2").value;
-              } else if (document.getElementById("PayOption3").checked) {
-                paymentOption = document.getElementById("PayOption3").value;
-              }
-
-              var prize = document.getElementById("totalAmount").innerHTML;
-              var itemCount = document.getElementById("itemCount").innerHTML;
-
-              orderDetails.push({
-                orderID: 'Ord-' + userID + Date.now(),
-                orderItems: cartDetails,
-                totalItems : itemCount,
-                totalAmount: prize,
-                deliveryAddress: selectedAddress,
-                deliveryDate: deliveryDate,
-                deliveryTime: deliveryTime,
-                paymentOption: paymentOption,
-                paymentStatus: 'Pending',
-                orderStatus: 'Pending',
-                orderDate: (new Date()).toString(),
-                orderBy: userID
-
-              });
-
-              console.log('insert order');
-              db.collection('OrderDetails')
-                .doc(userID)
-                .set({
-                  OrderDetails: orderDetails,
-                  CreatedBy: auth.currentUser.email,
-                  CreatedTimestamp: (new Date()).toString(),
-                  UpdatedBy: '',
-                  UpdatedTimestamp: ''
-                })
-                .then((docRef) => {
-                  console.log("Data added sucessfully in the document: ");
-                  //showAddress(false);
-                  //delete from cart after order places
-                  //cartDetails = [];
-                  db.collection('CartDetails')
-                    .doc(userID)
-                    .update({
-                      cartDetails: cartDetails
-                    })
-                    .then((docred) => {
-                      console.log('cart details made blank');
-
-                    });
-                  // console.log(Date.parse(eventstart))
-                })
-                .catch((error) => {
-                  console.log(error);
-                  //  console.error("error adding document:", error.message);
-                });
-
-
-            } else {
-              console.log('create order');
+            if (document.getElementById("delDate1").checked) {
+              deliveryDate = document.getElementById("delDate1").value;
+            } else if (document.getElementById("delDate2").checked) {
+              deliveryDate = document.getElementById("delDate2").value;
+            } else if (document.getElementById("delDate3").checked) {
+              deliveryDate = document.getElementById("delDate3").value;
+            } else if (document.getElementById("delDate4").checked) {
+              deliveryDate = document.getElementById("delDate4").value;
+            } else if (document.getElementById("delDate5").checked) {
+              deliveryDate = document.getElementById("delDate5").value;
+            } else if (document.getElementById("delDate6").checked) {
+              deliveryDate = document.getElementById("delDate6").value;
+            } else if (document.getElementById("delDate7").checked) {
+              deliveryDate = document.getElementById("delDate7").value;
+            }
+            var deliveryTime = '';
+            if (document.getElementById("delTime1").checked) {
+              deliveryTime = document.getElementById("delTime1").value;
+            } else if (document.getElementById("delTime2").checked) {
+              deliveryTime = document.getElementById("delTime2").value;
+            } else if (document.getElementById("delTime3").checked) {
+              deliveryTime = document.getElementById("delTime3").value;
+            } else if (document.getElementById("delTime4").checked) {
+              deliveryTime = document.getElementById("delTime4").value;
             }
 
+            var paymentOption = "";
+            if (document.getElementById("PayOption1").checked) {
+              paymentOption = document.getElementById("PayOption1").value;
+            } else if (document.getElementById("PayOption2").checked) {
+              paymentOption = document.getElementById("PayOption2").value;
+            } else if (document.getElementById("PayOption3").checked) {
+              paymentOption = document.getElementById("PayOption3").value;
+            }
 
-          });
+            var prize = document.getElementById("totalAmount").innerHTML;
+            var itemCount = document.getElementById("itemCount").innerHTML;
+            const snapshotOrder = db.collection('OrderDetails').doc(userID);
 
 
-        } else {
-          console.log('please select address');
-          //go to address
-        }
+          }
 
-
-      } else {
-        //go to add address
-        console.log('nned to add adress');
       }
 
 
     });
+  }
+    else {
+      console.log('please select address');
+      //go to address
+    }
+
 
   });
+  else {
+    //go to add address
+    console.log('nned to add adress');
+  }
+
+
+});
+
+});
 
 
 }
-
+*/
 function getCartSummary() {
   var arr = [];
   var prise = 0;
@@ -296,7 +408,6 @@ function getCartSummary() {
               var qty = cartItems[i].Quantity;
               var selectedsubItem = cartItems[i].SelectedsubItem;
               var weight = selectedsubItem.split('-');
-
               var selectedProduct = parr[parr.findIndex(e => e.ProductID === cartItems[i].ProductID)];
               if (selectedProduct.ProductDetails.findIndex(e => e.ProductWeight == weight[0].trim() >= 0)) {
                 var unitPrise = selectedProduct.ProductDetails[selectedProduct.ProductDetails.findIndex(e => e.ProductWeight == weight[0].trim())]
@@ -304,18 +415,14 @@ function getCartSummary() {
               }
             }
             var len = cartItems.length;
-            document.getElementById('itemCount').innerHTML = len ;
-
-            document.getElementById('totalAmount').innerHTML =  prise;
-
-
-
+            document.getElementById('itemCount').innerHTML = len;
+            document.getElementById('totalAmount').innerHTML = prise;
+            document.getElementById('CartitemCount').innerHTML = len;
           });
       } else {
         document.getElementById('itemCount').innerHTML = '0';
-
         document.getElementById('totalAmount').innerHTML = '0';
-
+        document.getElementById('CartitemCount').innerHTML ='0';
       }
     }
 
