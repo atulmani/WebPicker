739b080409cd8c00ad07fbc4121f1eaafc2d7c42
atulmani <atulmani@gmail.com>
2021-12-05 14:42:57 +0530

    Create updateProduct.js

---
+++ Agrikaash/manu/t1/t1.1/js/updateProduct.js
@@ -0,0 +1,233 @@
+//const productID = document.getElementById('productID');
+var userID = "";
+var cartItems = [];
+var productCategory = [];
+
+auth.onAuthStateChanged(firebaseUser => {
+  try {
+    if (firebaseUser) {
+      console.log('Logged-in user email id: ' + firebaseUser.email);
+      userID = firebaseUser.uid;
+      GetProfileData(firebaseUser);
+      document.getElementById('loading').style.display = 'none';
+
+      populateProductData('', '');
+      //promise2.then(console.log(productCategory));
+
+    } else {
+      console.log('User has been logged out');
+      window.location.href = "index.html";
+    }
+  } catch (error) {
+    console.log(error.message);
+    //window.location.href = "../index.html";
+  }
+});
+
+function GetProfileData(user) {
+  // const ref = db.collection("Users").doc(user.uid);
+
+  const snapshot = db.collection('Users').doc(user.uid);
+  snapshot.get().then(async (doc) => {
+      if (doc.exists) {
+        //console.log('Document ref id: ' + doc.data().uid);
+        userID = doc.data().uid;
+        document.getElementById('headerProfilePic').src = doc.data().ImageURL;
+        document.getElementById('displayName').innerHTML = doc.data().displayName;
+      }
+    })
+    .catch(function(error) {
+      // An error occurred
+      console.log(error.message);
+      // document.getElementById('errorMessage_Signup').innerHTML = error.message;
+      // document.getElementById('errorMessage_Signup').style.display = 'block';
+    });
+};
+
+function emptyDiv(div) {
+  div.innerHTML = '';
+}
+function populateProductData(bType, pType) {
+  //var DBrows = db.collection("Products").where("OrganizationId", "==", Number(organizationid)).get();
+  var divPType = document.getElementById('productCategory');
+  var divPList = document.getElementById('productRow');
+  emptyDiv(divPList);
+  emptyDiv(divPType);
+  var DBrows;
+  if ((pType === '' || pType === 'All') && (bType === '' || bType === 'All')) //Select all products
+  {
+    DBrows = db.collection("Products").get();
+  } else if ((pType === '' || pType === 'All') && (bType != '' && bType != 'All')) //select one customer businessType
+  {
+    DBrows = db.collection("Products").where("CustomerBusinessType", "==", bType).get();
+  } else if ((pType != '' && pType != 'All') && (bType === '' || bType === 'All')) //select one customer businessType
+  {
+    DBrows = db.collection("Products").where("productType", "==", pType).get();
+  } else if ((pType != '' && pType != 'All') && (bType != '' && bType != 'All')) //select one customer businessType
+  {
+    DBrows = db.collection("Products").where("productType", "==", pType).where("CustomerBusinessType", "==", bType).get();
+  }
+
+  DBrows.then((changes) => {
+    var item = [];
+    var index = 0;
+    var selectedindex = -1;
+    var selectdedItem;
+    productCategory.push('All');
+    changes.forEach(change => {
+      //if (change.type == 'added')
+      {
+        var pCategory = change.data().productType;
+        productCategory.push(pCategory)
+
+      }
+      renderProductNew(change, index);
+      index = index + 1;
+
+    });
+
+    var unique = productCategory.filter(onlyUnique);
+    renderProductCategory(unique);
+    document.getElementById('loading').style.display = 'none';
+
+    //  });
+  });
+}
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function renderProductCategory(productCategory) {
+
+  var div = document.getElementById('productCategory');
+  for (i = 0; i < productCategory.length; i++) {
+    var anchor = document.createElement("a");
+    anchor.setAttribute("href", "javascript:callFunction('" + productCategory[i] + "');");
+    anchor.innerHTML = productCategory[i];
+
+    var span = document.createElement("span");
+    span.innerHTML = "| ";
+
+    div.appendChild(anchor);
+    div.appendChild(span);
+  }
+}
+
+function callFunction(productType) {
+  document.getElementById('loading').style.display = 'block';
+  document.getElementById('hfpType').value = productType;
+
+  var e = document.getElementById("businessType");
+  var cType = e.options[e.selectedIndex].text;
+
+  // var cType = document.getElementById('businessType').SelectedValue;
+  populateProductData(cType, productType);
+  //renderProductNew(doc, index, selectedItem)
+}
+
+function changCustomerType() {
+  document.getElementById('loading').style.display = 'block';
+  var productType = document.getElementById('hfpType').value;
+  var e = document.getElementById("businessType");
+  var cType = e.options[e.selectedIndex].text;
+  populateProductData(cType, productType);
+  //renderProductNew(doc, index, selectedItem)
+}
+/////////////////////new function
+
+function renderProductNew(doc, index) {
+
+    var productlist = doc.data().ProductDetails;
+
+    var anchor = document.createElement("a");
+    anchor.setAttribute("href","createProduct.html?id=" +doc.id );
+
+    var div1 = document.createElement("div");
+    div1.setAttribute("class", "col-sm-12");
+    div1.setAttribute("style", "padding: 5px;");
+
+    var div1_1 = document.createElement("div");
+    div1_1.setAttribute("class", "product-list-div");
+
+    var table1 = document.createElement("table");
+    var tr1 = document.createElement("tr");
+
+    var td1 = document.createElement("td");
+    td1.setAttribute("width", "45%");
+    td1.setAttribute("class", "product-img-td");;
+    var hfID = document.createElement("input");
+    hfID.setAttribute("id", "hfID" + index);
+    hfID.setAttribute("type", "hidden");
+    hfID.setAttribute("value", doc.id);
+    td1.appendChild(hfID);
+    var img1 = document.createElement("img");
+    img1.setAttribute("src", doc.data().ProductImageURL);
+    img1.setAttribute("width", "100%")
+    img1.setAttribute("alt", "");
+    td1.appendChild(img1);
+
+    var div1_2 = document.createElement("div");
+    div1_2.setAttribute("class", "off-div");
+    div1_2.innerHTML = "<small>" + "20% OFF" + "</small>";
+    td1.appendChild(div1_2);
+
+    var div1_3 = document.createElement("div");
+    div1_3.setAttribute("class", "veg-nonVeg-div");
+
+    var imgVegNonVeg = document.createElement("img");
+    if (doc.data().VegNonVeg === "Veg")
+      imgVegNonVeg.setAttribute("src", "../img/veg.png");
+    else if (doc.data().VegNonVeg === "NonVeg")
+      imgVegNonVeg.setAttribute("src", "../img/non-veg.png");
+    imgVegNonVeg.setAttribute("width", "100%");
+    imgVegNonVeg.setAttribute("alt", "");
+
+    div1_3.appendChild(imgVegNonVeg);
+    td1.appendChild(div1_3);
+    tr1.appendChild(td1);
+
+    var td2 = document.createElement("td");
+    td2.setAttribute("width", "55%");
+    td2.setAttribute("valign", "top")
+    td2.setAttribute("class", "product-names-div");
+    td2.innerHTML = "<small class='product-names'>" + doc.data().ProductName + "</small><br>" +
+      "<small style='font-size: 0.8rem; color: rgba(0,0,0,0.5);'>" + doc.data().Brand + "</small><br>";
+
+    var selectP = document.createElement("select");
+    selectP.name = "productDetails";
+    selectP.id = "productDetails" + index;
+    //selectP.addEventListener("onchange", "mySelectionChange(" + "productDetails" + index + "," + "mrp" + index + "," + "final" + index + "," + "hfSelectedValue" + index + ","+index+")");
+    var indexnew = -1;
+    var mrp = 0;
+    var finalPrize = 0;
+    var qtyNew = doc.data().MinimumQty;
+    for (const val of productlist) {
+      var option = document.createElement("option");
+      option.value = val.ProductFinalPrise + ":" + val.ProductMRP;
+      option.text = val.ProductWeight + " - " + "Rs." + val.ProductFinalPrise;
+      selectP.appendChild(option);
+    }
+
+    td2.appendChild(selectP);
+    var div1_4 = document.createElement("div");
+    div1_4.setAttribute("class", "product-price");
+    //console.log(selectedItem);
+
+    // div1_4.innerHTML = "<h5>₹" + "<span id='mrp" + index + "' >" + productlist[0].ProductMRP + "</span>" + "</h5>" +
+    //   "<small>₹ " + "<span id='final" + index + "'>" + productlist[0].ProductFinalPrise + "</span></small>";
+
+      div1_4.innerHTML = "<large>" + "<span><b>Customer Business Type :  </b>" + doc.data().CustomerBusinessType  + "</span>" + "<large><br>" +
+        "<large>" + "<span><b>Product Type :  </b>" + doc.data().productType  + "</span>" + "<large><br>";
+
+
+
+    td2.appendChild(div1_4);
+    tr1.appendChild(td2);
+    table1.appendChild(tr1);
+    div1_1.appendChild(table1);
+    div1.appendChild(div1_1);
+    anchor.appendChild(div1);
+    document.getElementById("productRow").appendChild(anchor);
+
+}
