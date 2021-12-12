588e23d007d04651fdb428eab2d7cba382a3f038
atulmani <atulmani@gmail.com>
2021-12-13 00:26:27 +0530

    Update updateProduct.js

--- Agrikaash/manu/t1/t1.3/js/updateProduct.js
+++ Agrikaash/manu/t1/t1.3/js/updateProduct.js
@@ -3,7 +3,7 @@ var userID = "";
 var cartItems = [];
 var productCategory = [];
 var isAdmin = false;
-var userRole=[];
+var userRole = [];
 auth.onAuthStateChanged(firebaseUser => {
   try {
     if (firebaseUser) {
@@ -114,6 +114,7 @@ function populateProductData(bType, pType) {
 
     var unique = productCategory.filter(onlyUnique);
     renderProductCategory(unique);
+    anchorlLnkClick();
     document.getElementById('loading').style.display = 'none';
 
     //  });
@@ -128,8 +129,13 @@ function renderProductCategory(productCategory) {
 
   var div = document.getElementById('productCategory');
   for (i = 0; i < productCategory.length; i++) {
+    var idename = productCategory[i];
+    idename = idename.replace(" ", "");
+    console.log(idename);
     var anchor = document.createElement("a");
-    anchor.setAttribute("href", "javascript:callFunction('" + productCategory[i] + "');");
+    anchor.setAttribute("id", "anchor" + idename)
+    //anchor.setAttribute("href", "javascript:callFunction('" + productCategory[i] + "');");
+    //anchor.setAttribute("href", "javascript:callFunction('" + productCategory[i] + "');");
     anchor.innerHTML = productCategory[i];
 
     var span = document.createElement("span");
@@ -166,15 +172,16 @@ function renderProductNew(doc, index) {
 
   var productlist = doc.data().ProductDetails;
 
-  var anchor = document.createElement("a");
-  anchor.setAttribute("href", "createProduct.html?id=" + doc.id);
+  //var anchor = document.createElement("a");
+  //anchor.setAttribute("href", "createProduct.html?id=" + doc.id);
 
   var div1 = document.createElement("div");
   div1.setAttribute("class", "col-sm-12");
+  div1.setAttribute("id", "mainDiv" + index);
   div1.setAttribute("style", "padding: 5px;");
 
   var div1_1 = document.createElement("div");
-  div1_1.setAttribute("class", "product-list-div");
+  div1_1.setAttribute("class", "product-list-div " + doc.data().productType);
 
   var table1 = document.createElement("table");
   var tr1 = document.createElement("tr");
@@ -190,7 +197,7 @@ function renderProductNew(doc, index) {
   var img1 = document.createElement("img");
   img1.setAttribute("src", doc.data().ProductImageURL);
   img1.setAttribute("width", "100%")
-  img1.setAttribute("alt", "");
+  img1.setAttribute("alt", "Agrikaash");
   td1.appendChild(img1);
 
   var div1_2 = document.createElement("div");
@@ -210,7 +217,17 @@ function renderProductNew(doc, index) {
   imgVegNonVeg.setAttribute("alt", "");
 
   div1_3.appendChild(imgVegNonVeg);
+
+  var i1 = document.createElement("i");
+  i1.setAttribute("onclick", "GetProductDetails(" + "hfID" + index + ");");
+  i1.setAttribute("class", "far fa-edit address-edit-icon");
+  i1.setAttribute("style", "padding: 0 5px 0 5px;");
+
+  td1.appendChild(i1);
+
   td1.appendChild(div1_3);
+
+
   tr1.appendChild(td1);
 
   var td2 = document.createElement("td");
@@ -246,14 +263,79 @@ function renderProductNew(doc, index) {
   div1_4.innerHTML = "<large>" + "<span><b>Customer Business Type :  </b>" + doc.data().CustomerBusinessType + "</span>" + "<large><br>" +
     "<large>" + "<span><b>Product Type :  </b>" + doc.data().productType + "</span>" + "<large><br>";
 
+  var spanDelete = document.createElement('span');
+  spanDelete.setAttribute("id", "btnDelete" + index);
+  spanDelete.setAttribute("onclick", "deleteProduct(" + "hfID" + index + ",mainDiv" + index +");");
+  spanDelete.setAttribute("class", "material-icons");
+  spanDelete.setAttribute("style", "cursor:pointer;padding: 0 20px 0 5px;");
+  spanDelete.innerHTML = "delete_outline";
 
 
+
+  div1_4.appendChild(spanDelete);
   td2.appendChild(div1_4);
   tr1.appendChild(td2);
   table1.appendChild(tr1);
   div1_1.appendChild(table1);
   div1.appendChild(div1_1);
-  anchor.appendChild(div1);
-  document.getElementById("productRow").appendChild(anchor);
+
+  var anchor = document.createElement("a");
+  anchor.setAttribute("href", "createProduct.html?id=" + doc.id);
+
+  //  anchor.appendChild(div1);
+  document.getElementById("productRow").appendChild(div1);
+
+}
+
+
+function GetProductDetails(productID) {
+  console.log(productID.value);
+  window.location.href = "createProduct.html?id=" + productID.value;
+}
+
+function deleteProduct(productID, maindiv) {
+  db.collection("Products").doc(productID.value)
+    .delete();
+
+  document.getElementById("productRow").removeChild(maindiv);
+}
+
+function anchorlLnkClick() {
+
+  // $(window).load(function(){
+  console.log('Inside ready function');
+  console.log(document.getElementById('anchorAll'));
+  $('#anchorAll').click(function() {
+    // hideall();
+    // $('.all').toggle("slide");
+    console.log('clicked all');
+    $('.Goat').show("slide");
+    $('.Vegetable').show("slide");
+    $('.Pearl').show("slide");
+    $('.Fruit').show("slide");
+  });
+  $('#anchorGoatProducts').click(function() {
+    hideall();
+    $('.Goat').show("fadeUp");
+  });
+  $('#anchorVegetable').click(function() {
+    hideall();
+    $('.Vegetable').show("slide");
+  });
+  $('#anchorPearl').click(function() {
+    hideall();
+    $('.Pearl').show("slide");
+  });
+  $('#anchorFruit').click(function() {
+    hideall();
+    $('.Fruit').show("slide");
+  });
+
+  function hideall() {
+    $('.Goat').hide();
+    $('.Vegetable').hide();
+    $('.Pearl').hide();
+    $('.Fruit').hide();
+  };
 
 }
