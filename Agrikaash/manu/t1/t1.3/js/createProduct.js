99bd1031d7e698d3fb064e303feb4a7db004e3e1
atulmani <atulmani@gmail.com>
2021-12-13 12:56:34 +0530

    Update createProduct.js

--- Agrikaash/manu/t1/t1.3/js/createProduct.js
+++ Agrikaash/manu/t1/t1.3/js/createProduct.js
@@ -380,6 +380,26 @@ if(productID === null || productID === '')
         ImgUrl = url;
         alert('ImgUrl: ' + ImgUrl);
         console.log(productID);
+
+        //Update meta data for firebase storage resources - Start
+          var storageRef = uploadTask.snapshot.ref;
+          // console.log ("storageRef: " + storageRef );
+          // Create file metadata to update to cache for 1 year
+          var newMetadata = {
+            cacheControl: 'public,max-age=31536000',
+            contentType: 'image/png'
+          };
+          // Update metadata properties
+          storageRef.updateMetadata(newMetadata)
+            .then((metadata) => {
+              // Updated metadata for storage resources is returned in the Promise
+              console.log("metadata added on image url: " + url );
+            }).catch((error) => {
+              // Uh-oh, an error occurred!
+            });
+
+            //Update meta data for firebase storage resources - End
+
         db.collection("Products").doc(productID).update({
             // console.log('inside db collection: ' + newEventID);
             // EventId: newEventID,
