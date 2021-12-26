d6cdcd316e42a30197c6bfb48ea7fa616fbb3033
atulmani <atulmani@gmail.com>
2021-12-26 13:30:32 +0530

    Update Registration.js

--- Agrikaash/manu/t1/t1.5/js/Registration.js
+++ Agrikaash/manu/t1/t1.5/js/Registration.js
@@ -20,6 +20,20 @@ auth.onAuthStateChanged(firebaseUser => {
   }
 });
 
+const btnLogout = document.getElementById ('btnLogout');
+
+btnLogout.addEventListener('click', e => {
+  auth.signOut().then(() => {
+    // Sign-out successful.
+    window.location.href = "./login";
+    // console.log('Successfully Logged Out');
+  }).catch((error) => {
+    // An error happened.
+    // console.log('Error: ' + error.message);
+    window.location.href = "./login";
+  });
+});
+
 
 
 function GetProfileData() {
