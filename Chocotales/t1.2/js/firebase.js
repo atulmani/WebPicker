23747fb74e9cc0f4580dde00d173410ce6f4df0e
atulmani <atulmani@gmail.com>
2022-01-14 08:52:22 +0530

    Create firebase.js

---
+++ Chocotales/t1.2/js/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhHPegLrE8crjMW7SW53WPGVP_6vALDXo",
+  authDomain: "choco-tales.firebaseapp.com",
+  projectId: "choco-tales",
+  storageBucket: "choco-tales.appspot.com",
+  messagingSenderId: "80137567579",
+  appId: "1:80137567579:web:57a6a015e2678d824a2e7e",
+  measurementId: "G-S9BWTDH7CP"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
