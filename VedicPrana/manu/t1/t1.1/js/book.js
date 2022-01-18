912b703f74084d98fd0666775ba555823935baf6
atulmani <atulmani@gmail.com>
2022-01-18 13:17:58 +0530

    Create book.js

---
+++ VedicPrana/manu/t1/t1.1/js/book.js
@@ -0,0 +1,15 @@
+
+function menuListClassClick() {
+  var menuListHr = document.getElementById('menuListHr');
+  menuListHr.style.transform = 'translateX(0%)';
+};
+
+function menuListCourseClick() {
+  var menuListHr = document.getElementById('menuListHr');
+  menuListHr.style.transform = 'translateX(100%)';
+};
+
+function menuListAppointmentsClick() {
+  var menuListHr = document.getElementById('menuListHr');
+  menuListHr.style.transform = 'translateX(200%)';
+};
