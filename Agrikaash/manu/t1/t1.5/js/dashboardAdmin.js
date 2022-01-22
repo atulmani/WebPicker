89c784a7bfbd6f13f151d3e4af317ece781f3965
atulmani <atulmani@gmail.com>
2022-01-22 09:51:42 +0530

    Update dashboardAdmin.js

--- Agrikaash/manu/t1/t1.5/js/dashboardAdmin.js
+++ Agrikaash/manu/t1/t1.5/js/dashboardAdmin.js
@@ -283,7 +283,8 @@ function PopulateOrderSummary() {
         document.getElementById("cardDelivery").style.display = "block";
         // document.getElementById("productDiv").style.display = "block";
         //document.getElementById("userRegistration").style.display = "block";
-        // document.getElementById("trendChart").style.display = "block";
+        document.getElementById("trendChart").style.height = "450px";
+        document.getElementById("trendChart").style.visibility = "visible";
         getLastOrder();
       });
       if (flag === false) {
