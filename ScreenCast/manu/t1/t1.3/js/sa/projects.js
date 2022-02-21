5f3be6d915bf2ff490b140feab33ea82d38e6ec0
atulmani <atulmani@gmail.com>
2022-02-21 20:51:05 +0530

    Update projects.js

--- ScreenCast/manu/t1/t1.3/js/sa/projects.js
+++ ScreenCast/manu/t1/t1.3/js/sa/projects.js
@@ -1,13 +1,26 @@
 
-
-function projectMenuListClick(filter){
+function projectMenuListClick(filter) {
   var projectMenuListHr = document.getElementById('projectMenuListHr');
 
-  if(filter === 'Active') {
+  if (filter === 'PROPOSAL') {
     projectMenuListHr.style.transform = 'translateX(0%)';
-  } else if (filter === 'Pending') {
+  } else if (filter === 'SUBMITTED') {
     projectMenuListHr.style.transform = 'translateX(100%)';
-  } else {
+  } else if (filter === 'ACTIVE') {
     projectMenuListHr.style.transform = 'translateX(200%)';
+  } else {
+    projectMenuListHr.style.transform = 'translateX(300%)';
   }
 }
+
+var btnCreateProject = document.getElementById('btnCreateProject');
+
+btnCreateProject.addEventListener('click', function(e) {
+  try {
+    window.location.href = "projectdetails.html?id=";
+    // var siteURL = localStorage.getItem("siteURL");
+    // localStorage.setItem("siteURL", siteURL);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
