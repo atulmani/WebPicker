a54926adb1122c14ba3afec65c5c0935ca6b1da5
atulmani <atulmani@gmail.com>
2022-03-12 17:12:41 +0530

    Update partners.js

--- ScreenCast/manu/t1/t1.3/js/sa/partners.js
+++ ScreenCast/manu/t1/t1.3/js/sa/partners.js
@@ -1,6 +1,12 @@
+
+// var partnerList = '';
+// var callCount = '';
+// var changeSelect = '';
+
 auth.onAuthStateChanged(firebaseUser => {
   try {
-    populatePartnersList();
+    updateIntellisense();
+    populatePartnersList(true);
     if (firebaseUser) {
       // document.getElementById('displayName').innerHTML = firebaseUser.displayName;
       // document.getElementById('customerProfileIcon').src = firebaseUser.photoURL;
@@ -9,6 +15,7 @@ auth.onAuthStateChanged(firebaseUser => {
       // GetProfileData(firebaseUser);
 
       // getSiteURL();
+
 
 
 
@@ -24,7 +31,7 @@ auth.onAuthStateChanged(firebaseUser => {
   }
 });
 
-function populatePartnersList() {
+function populatePartnersList(flag) {
 
   var options = {
     year: 'numeric',
@@ -39,18 +46,24 @@ function populatePartnersList() {
 
       changes.forEach(doc => {
 
+        var specification = '';
+
+        if (doc.data().partnerType === 'CLIENT') {
+          specification = 'C';
+        } else if (doc.data().partnerType === 'VENDOR') {
+          specification = 'V';
+        }
+
         var odeldate = new Date(doc.data().CreatedTimestamp.seconds * 1000);
         var delDate  = odeldate.toLocaleDateString("en-US", options);
 
-        console.log(delDate);
-
         let partnersCard = document.createElement('a');
         partnersCard.setAttribute('href', 'partnerDetails.html?id=' + doc.id);
-        partnersCard.setAttribute('class', 'ticket-card');
+        partnersCard.setAttribute('class', 'ticket-card ' + doc.data().partnerStatus + ' ' + doc.data().partnerType + '');
 
           partnersCard.innerHTML =
           '<div class="">' +
-            '<img src="../img/clients/8.png" alt="">' +
+            '<img src="' + doc.data().PartnerImgURL + '" alt="">' +
           '</div>' +
           '<div class="details">' +
             '<small>Partner ID: ' + doc.data().partnerID + '</small>' +
@@ -58,60 +71,87 @@ function populatePartnersList() {
             '<span>' + doc.data().partnerLocation + '</span>' +
             '<br>' +
             '<small>' + delDate + '</small>' +
+          '</div>' +
+          '<div class="specification">' +
+            '<span>' + specification + '</span>' +
           '</div>';
 
           document.getElementById('populateDataDiv').appendChild(partnersCard);
 
-          // paymentCard.innerHTML =
-          //   '<div class="content-div">' +
-          //   '<div width="60%">' +
-          //   '<div class="name-price">' +
-          //   '<h5>' + change.doc.data().Client + '</h5>' +
-          //   '<small>' + pDate + '</small>' +
-          //   // '<small>' + pDate + '</small>' +
-          //   '</div>' +
-          //   '</div>' +
-          //
-          //   '<div style="text-align:right;" width="40%">' +
-          //   '<div class="price-type ' + amountTextColor + '">' +
-          //   '<h6>₹ ' + change.doc.data().Amount + '</h6>' +
-          //   '<small>Paid by ' + change.doc.data().ModeOfPayment + '</small>' +
-          //   '</div>' +
-          //   '</div>' +
-          //   '</div>' +
-          //
-          //   '<div style="width:100%;">' +
-          //   '<span class="payment-para1">' + shortComment + '... </span>' +
-          //   '<span class="show-more">show more</span>' +
-          //   '<a href="addPayments.html?id=' + change.doc.id + '">' +
-          //   '<span class="material-icons-outlined edit-icon">edit</span>' +
-          //   '</a>' +
-          //   '<div class="payment-para2">' +
-          //   '<span>' + txtComment + '</span>' +
-          //   '</div>' +
-          //   '</div>';
+      });
+      if (flag === true) {
+        menuListClick('1');
+      }
 
-      })
-
-    })
+    });
 }
 
-
+function hideall(){
+  $('.CLIENT').hide();
+  $('.VENDOR').hide();
+  $('.INACTIVE').hide();
+};
 
 function menuListClick(filter) {
+  document.getElementById("wrongSearch").style.display="none";
+  var searchDivInput = document.getElementById("searchDivInput").value;
+  var opartnerLocationSelect = document.getElementById("partnerLocationSelect");
+  var partnerLocationSelect = opartnerLocationSelect.options[opartnerLocationSelect.selectedIndex].value;
   var MenuListHr = document.getElementById('partnerMenuListHr');
-
-  if (filter === 'CLIENTS') {
+  document.getElementById("clientTypeFilter").value=filter;
+  hideall();
+  console.log($('.CLIENT').length);
+  if (filter === '1') {
+    $('.CLIENT.ACTIVE').show("slide");
+    if($('.CLIENT.ACTIVE').length === 0 )
+    {
+      if(searchDivInput === "")
+      {
+        document.getElementById("searchKeyText").innerHTML=" Client ";
+      }
+      else{
+        document.getElementById("searchKeyText").innerHTML= searchDivInput + " as Client ";
+      }
+      document.getElementById("wrongSearch").style.display="block";
+    }
     MenuListHr.style.transform = 'translateX(0%)';
-  } else if (filter === 'VENDORS') {
+  } else if (filter === '2') {
+    $('.VENDOR.ACTIVE').show("slide");
+    if($('.VENDOR.ACTIVE').length === 0 )
+    {
+      if(searchDivInput === "")
+      {
+        document.getElementById("searchKeyText").innerHTML=" Vendor ";
+      }
+      else{
+        document.getElementById("searchKeyText").innerHTML= searchDivInput + " as Vendor ";
+      }
+      document.getElementById("wrongSearch").style.display="block";
+    }
     MenuListHr.style.transform = 'translateX(100%)';
-  } else if (filter === 'INACTIVE') {
+  } else if (filter === '3') {
+    $('.INACTIVE').show("slide");
+    if($('.INACTIVE').length === 0 )
+    {
+        if(searchDivInput === "")
+        {
+          document.getElementById("searchKeyText").innerHTML=" Inactive ";
+        }
+        else{
+          document.getElementById("searchKeyText").innerHTML= searchDivInput + " as Inactive ";
+        }
+      document.getElementById("wrongSearch").style.display="block";
+    }
     MenuListHr.style.transform = 'translateX(200%)';
-    // } else {
-    //   MenuListHr.style.transform = 'translateX(300%)';
-    // }
+  };
+
+  if (partnerLocationSelect != 'All') {
+    document.getElementById("searchKeyText").innerHTML=document.getElementById("searchKeyText").innerHTML + " in " + partnerLocationSelect;
   }
+
 }
+
+
 var btnCreatePartner = document.getElementById('btnCreatePartner');
 
 btnCreatePartner.addEventListener('click', function(e) {
@@ -124,3 +164,263 @@ btnCreatePartner.addEventListener('click', function(e) {
     console.error(err.message);
   }
 });
+
+function toggleSearchInput() {
+  var searchDiv = document.getElementById('searchDiv');
+  var searchDivInput = document.getElementById('searchDivInput');
+  searchDiv.classList.toggle('open');
+
+  if (searchDiv.classList.contains('open')) {
+    searchDivInput.focus();
+  } else {
+    searchDivInput.blur();
+  }
+}
+
+function inputSearchFocus() {
+  var intellisense = document.getElementById('intellisense');
+  var searchDivInput = document.getElementById('searchDivInput');
+
+  intellisense.style.width = '100%';
+  intellisense.style.height = '200px';
+  intellisense.style.pointerEvents = 'all';
+  intellisense.style.opacity = '1';
+
+  searchDivInput.style.color = '#666';
+}
+
+
+function inputSearchBlur() {
+  var intellisense = document.getElementById('intellisense');
+  var searchDivInput = document.getElementById('searchDivInput');
+
+  intellisense.style.width = '0';
+  intellisense.style.height = '0';
+  intellisense.style.opacity = '0';
+  searchDivInput.style.color = '#fff';
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("searchDivInput");
+  filter = input.value.toUpperCase();
+  //  console.log(filter);
+  div = document.getElementById("intellisense");
+  a = div.getElementsByTagName("div");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+function updateIntellisense(){
+
+  var intellisense = document.getElementById('intellisense');
+
+  db.collection("Partners")
+  .get()
+    .then((changes) => {
+      changes.forEach(doc => {
+        var intellisenseItem = document.createElement('div');
+        intellisenseItem.setAttribute('onclick', 'searchIntellisenseClick(' + doc.id + ')');
+        intellisenseItem.innerHTML = doc.data().partnerName;
+
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('value', doc.data().partnerName);
+        input.setAttribute('id', doc.id);
+
+        intellisenseItem.appendChild(input);
+        intellisense.appendChild(intellisenseItem);
+      })
+    });
+}
+
+function searchIntellisenseClick(docID){
+  document.getElementById("wrongSearch").style.display="none";
+  var partnerList = [];
+  partnerList.push(docID.id);
+  document.getElementById("searchDivInput").value = docID.value;
+  document.getElementById('populateDataDiv').innerHTML="";
+  document.getElementById('partnerLocationSelect').options[0].selected=true;
+  RenderPartnerByPartnerID(partnerList, 0);
+  // RenderPartnerByPartnerID(changeSelect, partnerList, 0);
+}
+
+function myChangeEvent() {
+  console.log('myChnageEvent');
+  document.getElementById("wrongSearch").style.display="none";
+
+  document.getElementById("populateDataDiv").innerHTML = "";
+  var noFlag= false;
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("searchDivInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("intellisense");
+  a = div.getElementsByTagName("div");
+  var hfid = "";
+  var partnerList = [];
+  var partnerCnt = 0;
+  var callCount = 1;
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      noFlag = true;;
+      a[i].style.display = "";
+      hfid = a[i].getElementsByTagName("input")[0];
+      partnerList.push(hfid.id);
+      partnerCnt = partnerCnt + 1;
+      if (partnerCnt === 10) {
+        RenderPartnerByPartnerID(partnerList, callCount);
+        // RenderPartnerByPartnerID(changeSelect, partnerList, callCount);
+        partnerList = [];
+        partnerCnt = 0;
+        callCount = callCount + 1;
+      }
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+  if (partnerList.length > 0) {
+
+    RenderPartnerByPartnerID(partnerList, callCount);
+    // RenderPartnerByPartnerID(changeSelect, partnerList, callCount);
+  }
+  console.log(noFlag);
+  if(noFlag === false)
+  {
+    document.getElementById("searchKeyText").innerHTML = filter;
+    document.getElementById("wrongSearch").style.display="block";
+  }
+  console.log("before display none");
+  document.getElementById("searchDivInput").blur();
+  // myDropdown.classList.remove("show");
+  // serachDiv.classList.remove("open");
+}
+
+function changePartnerLocation() {
+  document.getElementById('searchDivInput').value = '';
+  var partnerLocationSelect = document.getElementById('partnerLocationSelect');
+  var changeSelect = partnerLocationSelect.options[partnerLocationSelect.selectedIndex].text;
+
+  console.log(changeSelect);
+
+  RenderPartnerByLocation(changeSelect);
+
+}
+
+function RenderPartnerByPartnerID(partnerList, callCount) {
+
+  var options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  };
+  var partnerType="";
+  var firstFlag =true;
+  var DBrows = db.collection("Partners")
+    .where("__name__", "in", partnerList)
+    .get();
+  DBrows.then((changes) => {
+    changes.forEach(doc => {
+
+      var odeldate = new Date(doc.data().CreatedTimestamp.seconds * 1000);
+      var delDate  = odeldate.toLocaleDateString("en-US", options);
+      console.log('in for loop');
+      let partnersCard = document.createElement('a');
+      partnersCard.setAttribute('href', 'partnerDetails.html?id=' + doc.id);
+      partnersCard.setAttribute('class', 'ticket-card ' + doc.data().partnerStatus + ' ' + doc.data().partnerType + '');
+      if(firstFlag === true)
+      {
+        partnerType=doc.data().partnerType;
+        console.log(partnerType);
+        firstFlag = false;
+      }
+        partnersCard.innerHTML =
+        '<div class="">' +
+          '<img src="' + doc.data().PartnerImgURL + '" alt="">' +
+        '</div>' +
+        '<div class="details">' +
+          '<small>Partner ID: ' + doc.data().partnerID + '</small>' +
+          '<h5>' + doc.data().partnerName + '</h5>' +
+          '<span>' + doc.data().partnerLocation + '</span>' +
+          '<br>' +
+          '<small>' + delDate + '</small>' +
+        '</div>';
+        console.log(partnersCard.innerHTML);
+        document.getElementById('populateDataDiv').appendChild(partnersCard);
+
+    });
+    console.log(partnerType);
+    if(partnerType === 'CLIENT'){
+      menuListClick("1");
+    }else if(partnerType === 'VENDOR'){
+      menuListClick("2");
+    }
+
+
+  });
+
+  var searchDiv = document.getElementById('searchDiv');
+  searchDiv.classList.remove("open");
+
+}
+
+function RenderPartnerByLocation(changeSelect) {
+
+  var options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  };
+  var clientTypeFilter = document.getElementById("clientTypeFilter").value;
+
+  document.getElementById('populateDataDiv').innerHTML = '';
+
+  var DBrows;
+
+  if (changeSelect === 'All') {
+   DBrows = db.collection("Partners")
+    .get();
+  } else {
+    DBrows = db.collection("Partners")
+     .where("partnerLocation", "==", changeSelect)
+     .get();
+  }
+
+  DBrows.then((changes) => {
+    changes.forEach(doc => {
+
+      var odeldate = new Date(doc.data().CreatedTimestamp.seconds * 1000);
+      var delDate  = odeldate.toLocaleDateString("en-US", options);
+
+      let partnersCard = document.createElement('a');
+      partnersCard.setAttribute('href', 'partnerDetails.html?id=' + doc.id);
+      partnersCard.setAttribute('class', 'ticket-card ' + doc.data().partnerStatus + ' ' + doc.data().partnerType + '');
+
+        partnersCard.innerHTML =
+        '<div class="">' +
+          '<img src="../img/clients/8.png" alt="">' +
+        '</div>' +
+        '<div class="details">' +
+          '<small>Partner ID: ' + doc.data().partnerID + '</small>' +
+          '<h5>' + doc.data().partnerName + '</h5>' +
+          '<span>' + doc.data().partnerLocation + '</span>' +
+          '<br>' +
+          '<small>' + delDate + '</small>' +
+        '</div>';
+        console.log(partnersCard.innerHTML);
+        document.getElementById('populateDataDiv').appendChild(partnersCard);
+
+    });
+    menuListClick(clientTypeFilter);
+  });
+
+  var searchDiv = document.getElementById('searchDiv');
+  searchDiv.classList.remove("open");
+
+}
