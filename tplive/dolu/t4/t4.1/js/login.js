8499bcfc16886f8ca32656671f8e4364a2807f1d
Sanidhya-ctrl <72963786+Sanidhya-ctrl@users.noreply.github.com>
2022-03-19 17:10:40 +0530

    Create login.js

---
+++ tplive/dolu/t4/t4.1/js/login.js
@@ -0,0 +1,51 @@
+
+window.onload = function(){
+  render();
+}
+
+function render()
+{
+  console.log('Captcha rendered starts taking place');
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+  recaptchaVerifier.render();
+  console.log('Captcha rendered');
+}
+
+var btnSendOTP = document.getElementById('btnSendOTP');
+
+btnSendOTP.addEventListener('click', e => {
+  // e.preventDefault();
+// function phoneAuth() {
+  var number = document.getElementById('userPhoneNo').value;
+  var number = "+91" + number;
+  console.log('Phone No: ' + number);
+  auth.signInWithPhoneNumber(number, window.recaptchaVerifier)
+  .then(function(confirmationResult){
+    window.confirmationResult = confirmationResult;
+    coderesult = confirmationResult;
+    console.log('coderesult: ' + coderesult);
+    console.log('confirmationResult.verificationId ' + confirmationResult.verificationId);
+    console.log('Message sent');
+  })
+  .catch(function(error){
+    console.log('Error Sending OTP: ' + error.message);
+  })
+});
+
+var btnSigninUsingOTP = document.getElementById('btnSigninUsingOTP');
+
+btnSigninUsingOTP.addEventListener('click', e => {
+  e.preventDefault();
+  var code = document.getElementById('txtVerificationCode').value;
+  console.log('Code: ' + code);
+  coderesult.confirm(code).then(function (result) {
+    console.log('Navigate to the dashboard/profile page');
+    window.location.assign('../admin/dashboard.html');
+    var  user = result.user;
+    console.log(user);
+  })
+  .catch(function(error){
+    // alert(error.message);
+    console.error(error);
+  });
+});
