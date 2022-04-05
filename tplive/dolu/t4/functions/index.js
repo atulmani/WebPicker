9d3c8ce16fbfc84880e1479cb548ef513246ab94
atulmani <atulmani@gmail.com>
2022-04-05 23:31:54 +0530

    Create index.js

---
+++ tplive/dolu/t4/functions/index.js
@@ -0,0 +1,22 @@
+// const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+const userProfile = require("./profile.js");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+exports.getProfileDetails = userProfile.getProfileDetails;
+exports.getUserRequest = userProfile.getUserRequest;
+exports.addUserDetails = userProfile.addUserDetails;
+exports.saveProfileDetailsStep1 = userProfile.saveProfileDetailsStep1;
+exports.saveProfileDetailsStep2 = userProfile.saveProfileDetailsStep2;
+exports.saveProfileDetailsStep3 = userProfile.saveProfileDetailsStep3;
+exports.saveProfileDetailsStep4 = userProfile.saveProfileDetailsStep4;
+exports.updateProfileDetails = userProfile.updateProfileDetails;
