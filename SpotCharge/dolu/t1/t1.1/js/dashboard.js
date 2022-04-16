eb0b3015fe6b1e696bbcce4171b7f3e9e989b523
Sanidhya-ctrl <72963786+Sanidhya-ctrl@users.noreply.github.com>
2022-04-17 01:53:04 +0530

    Create dashboard.js

---
+++ SpotCharge/dolu/t1/t1.1/js/dashboard.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+        if($(window).width() > 850) {
+           // $("#collapsibleNavbar1").removeClass("collapse");
+           $("#arrowOrg").addClass("active");
+           $("#eventArrow").addClass("active");
+           $("#EventRevenue").addClass("active");
+           $("#openEvents").addClass("active");
+           $("#CardTransaction").addClass("active");
+           $("#CardNotification").addClass("active");
+           $("#CardAnalytics").addClass("active");
+        }
+    });
+
+function fullcard(arrowVar)
+{
+  // console.log(arrowVar).;
+  arrowVar.classList.toggle('active');
+
+}
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
