8c3c7a4c0e05bc4c38ea7b7babd08028089a0bdb
atulmani <atulmani@gmail.com>
2022-04-23 12:03:58 +0530

    profile

--- SpotCharge/dolu/t1/t1.1/js/main_manu.js
+++ SpotCharge/dolu/t1/t1.1/js/main_manu.js
@@ -1,29 +1,7 @@
-// Ristrict mouse right click - start
-document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-}, false);
-
-// With jQuery
-$(document).on({
-    "contextmenu": function(e) {
-        console.log("ctx menu button:", e.which);
-
-        // Stop the context menu
-        e.preventDefault();
-    },
-    "mousedown": function(e) {
-        console.log("normal mouse down:", e.which);
-    },
-    "mouseup": function(e) {
-        console.log("normal mouse up:", e.which);
-    }
-});
-
-// Ristrict mouse right click - end
 
 // Collapse Navbar toggle menu after clicking on menu items - Start
 $(document).on('click', function() {
-  $('.collapse').collapse('hide');
+  document.getElementById('sideNavbar').classList.remove('open');
 });
 // Collapse Navbar toggle menu after clicking on menu items - End
 
@@ -46,53 +24,26 @@ $(document).on('click', function() {
     });
    // fixed-top or sticky-top navbar background change on windows scrolling - end
 
+   // sideNavbar - start
+
+   var hamberIcon = document.getElementById('hamberIcon');
+   var closeBtn = document.getElementById('closeBtn');
+   var sideNavbar = document.getElementById('sideNavbar');
+
+   hamberIcon.addEventListener('click', sideNavbarOpen, false);
+
+   function sideNavbarOpen(){
+     sideNavbar.classList.add('open');
+   }
+
+   closeBtn.addEventListener('click', sideNavbarClose, false);
+
+   function sideNavbarClose(){
+     sideNavbar.classList.remove('open');
+   }
 
 
-// jQuery(document).ready(function(){
-//
-//     $('#videolink').magnificPopup({
-//         type:'inline',
-//         midClick:true
-//
-//     });
-// });
-
-// $(function () {
-//     $("#team-members").owlCarousel({
-//         items: 2,
-//         autoplay: true,
-//         smartSpeed: 700,
-//         loop: true,
-//         autoplayHoverPause: true,
-//         nav: true,
-//         dots: false,
-//         responsive: {
-//             0: {
-//                 items: 1
-//             },
-//             480: {
-//                 items: 2
-//             }
-//         }
-//     });
-// });
-
-
-  // $(document).ready(function(){
-  //     $("#testimonial-slider").owlCarousel({
-  //        items:1,
-  //         itemsDesktop:[1000,1],
-  //         itemsDesktopSmall:[979,1],
-  //         itemsTablet:[768,1],
-  //         pagination:true,
-  //         autoPlay:false
-  //
-  //     });
-  //    $('.count').counterUp({
-  //               delay: 10,
-  //               time: 4000
-  //           });
-  // });
+   // sideNavbar - end
 
   $(document).ready(function() {
     var owl = $('#vehicle-list');
@@ -101,11 +52,12 @@ $(document).on('click', function() {
       loop: false,
       autoplay: false,
       smartSpeed: 1000,
-      // autoplayTimeout: 2000,
+      autoplayTimeout: 4000,
       autoplayHoverPause: true,
       nav:false,
       dots:false,
       stagePadding: 30,
+      rewind: true,
       responsive: {
         0: {
           items: 2
@@ -146,55 +98,3 @@ $(document).on('click', function() {
         }
       });
     });
-
-// $(function(){
-//     $("#clients-list").owlCarousel({
-//        items:6,
-//         autoPlay:false,
-//         smartSpeed:700,
-//         loop:true,
-//         autoplayHoverPause:true,
-//         nav:false,
-//         dots:false,
-//
-//         responsive:{
-//             0:{
-//                 items:3
-//             },
-//
-//              480:{
-//                 items:3
-//             },
-//
-//              768:{
-//                 items:6
-//             }
-//         }
-//     });
-
-    // $("#news-slider").owlCarousel({
-    //     items:3,
-    //     itemsDesktop:[1199,3],
-    //     itemsDesktopSmall:[1000,2],
-    //     itemsMobile:[600,1],
-    //     pagination:false,
-    //     navigationText:false,
-    //     autoPlay:true
-    //
-    //
-    // });
-
-    // $("#banner-slider").owlCarousel({
-    //     items: 1,
-    //     itemsDesktop: [1199, 1],
-    //     itemsDesktopSmall: [1000, 1],
-    //     itemsMobile: [600, 1],
-    //     pagination: false,
-    //     navigationText: false,
-    //     autoPlay: true
-    //
-    //
-    // });
-
-
-// });
--- SpotCharge/dolu/t1/t1.1/js/profile_manu.js
+++ SpotCharge/dolu/t1/t1.1/js/profile_manu.js
@@ -1,15 +1,1 @@
-var hamberIcon = document.getElementById('hamberIcon');
-var closeBtn = document.getElementById('closeBtn');
-var sideNavbar = document.getElementById('sideNavbar');
 
-hamberIcon.addEventListener('click', sideNavbarOpen, false);
-
-function sideNavbarOpen(){
-  sideNavbar.classList.add('open');
-}
-
-closeBtn.addEventListener('click', sideNavbarClose, false);
-
-function sideNavbarClose(){
-  sideNavbar.classList.remove('open');
-}
