2e20e52c35758bfdc4b9d614259e3a005bc01274
atulmani <atulmani@gmail.com>
2022-04-26 01:05:22 +0530

    Update eventDetail.js

--- tplive/dolu/t4/t4.2/js/eventDetail.js
+++ tplive/dolu/t4/t4.2/js/eventDetail.js
@@ -1,12 +1,62 @@
 let eventDocUrl = new URL(location.href);
 let searchParams = new URLSearchParams(eventDocUrl.search);
 var eventID = searchParams.get('id');
+var loggedinUser = "";
 
 window.onload = function() {
   // yourFunction(param1, param2);
   GetEventDetails();
+  GetEventCategory();
+
+  localStorage.setItem("EventID",eventID);
+
 };
 
+auth.onAuthStateChanged(async firebaseUser => {
+  try {
+    if (firebaseUser) {
+      loggedinUser = firebaseUser;
+      //console.log(firebaseUser.uid);
+      console.log('Logged-in user phone number: ' + loggedinUser.phoneNumber);
+
+      GetProfileData();
+    } else {
+      loggedinUser = null;
+
+    }
+
+  } catch (error) {
+    console.log(error.message);
+    // window.location.href = "../index.html";
+  }
+});
+
+async function GetProfileData() {
+  console.log('GetProfileData - Starts');
+  var userProfile = JSON.parse(localStorage.getItem("userProfile"));
+  // if (userProfile != undefined && userProfile != "" && userProfile != null) {
+  //   if (userProfile.id != "0") {
+  //     document.getElementById("userName").innerHTML = userProfile.UserName;
+  //     document.getElementById("userlocation").innerHTML = userProfile.City;
+  //
+  //     if (userProfile.UserRole.findIndex(e => e.TYPE === "ADMIN") >= 0) {
+  //       console.log("in admin");
+  //     } else if (userProfile.UserRole.findIndex(e => e.TYPE === "ORGANIZER") >= 0) {
+  //       console.log("organizer");
+  //       // document.getElementById("fInput").style.display="none";
+  //     } else {
+  //       console.log("not admin");
+  //       document.getElementById("containerOrgList").style.display = "none";
+  //       document.getElementById("errorMessage").style.display = "block";
+  //     }
+  //   } else {
+  //     console.log("not admin");
+  //     document.getElementById("containerOrgList").style.display = "none";
+  //     document.getElementById("errorMessage").style.display = "block";
+  //   }
+  // }
+
+}
 
 
 function GetEventDetails() {
@@ -25,9 +75,11 @@ function GetEventDetails() {
     var record1 = result.data;
     console.log(result.data);
 
+    localStorage.setItem("eventDetails", JSON.stringify(result.data));
+
     document.getElementById("hfEventID").value = result.data.Eventid;
     document.getElementById("hfOrganizerID").value = result.data.OrganizerID;
-    document.getElementById("eventName").innerHTML= result.data.EventName;
+    document.getElementById("eventName").innerHTML = result.data.EventName;
     document.getElementById("organisername").innerHTML = result.data.EventOwnerName;
 
     if (result.data.EventStartDate != "" && result.data.EventStartDate != undefined && result.data.EventStartDate != null)
@@ -35,26 +87,26 @@ function GetEventDetails() {
     if (result.data.EventEndDate != "" && result.data.EventEndDate != undefined && result.data.EventEndDate != null)
       document.getElementById("eventenddate").innerHTML = new Date(result.data.EventEndDate._seconds * 1000).toLocaleDateString("en-US", options);
 
-      document.getElementById("eventprice").innerHTML = "";
-      // document.getElementById("eventprice").value = result.data.EventPrize;
+    document.getElementById("eventprice").innerHTML = result.data.MinimumFee + " - " + result.data.MaximumFee;
+    // document.getElementById("eventprice").value = result.data.EventPrize;
 
-document.getElementById("eventVenue").innerHTML = result.data.EventVenue;
-document.getElementById("eventCategory").innerHTML = "bs;Bd";
-document.getElementById("eventstatus").innerHTML = "Upcoming";
-document.getElementById("organiserEmail").innerHTML = "Upcoming";
+    document.getElementById("eventVenue").innerHTML = result.data.EventVenue;
+    //document.getElementById("eventCategory").innerHTML = "bs;Bd";
+    document.getElementById("eventstatus").innerHTML = "Upcoming";
+    document.getElementById("organiserEmail").innerHTML = "Upcoming";
 
-if (result.data.WithdrawalEndDate != "" && result.data.WithdrawalEndDate != undefined && result.data.WithdrawalEndDate != null)
-  document.getElementById("withdrawalDate").innerHTML = new Date(result.data.WithdrawalEndDate._seconds * 1000).toLocaleDateString("en-US", options);
+    if (result.data.WithdrawalEndDate != "" && result.data.WithdrawalEndDate != undefined && result.data.WithdrawalEndDate != null)
+      document.getElementById("withdrawalDate").innerHTML = new Date(result.data.WithdrawalEndDate._seconds * 1000).toLocaleDateString("en-US", options);
 
-document.getElementById("organiserEmail").innerHTML = result.data.EventOwnerEmail;
+    document.getElementById("organiserEmail").innerHTML = result.data.EventOwnerEmail;
 
-if (result.data.OrganizerPhone != undefined)
-  document.getElementById("organiserPhone").innerHTML = result.data.EventOwnerPhone;
+    if (result.data.OrganizerPhone != undefined)
+      document.getElementById("organiserPhone").innerHTML = result.data.EventOwnerPhone;
 
 
-document.getElementById("RulesAndRegulation").innerHTML = result.data.RulesAndRegulations;
+    document.getElementById("RulesAndRegulation").innerHTML = result.data.RulesAndRegulations;
 
-///////////
+    ///////////
     // var sportName = document.getElementById("ddlSports");
     // var sportNameVal = result.data.SportName;
     // for (index = 0; index < sportName.options.length; index++) {
@@ -156,3 +208,65 @@ document.getElementById("RulesAndRegulation").innerHTML = result.data.RulesAndRe
     // }
   });
 }
+
+
+
+function GetEventCategory() {
+  console.log(eventID);
+  var options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  };
+
+  var para1 = {};
+  para1 = {
+    EventID: eventID
+  };
+  var catList ="";
+  console.log(para1);
+  const ret1 = firebase.functions().httpsCallable("getEventCategoryDetails");
+  ret1(para1).then((result) => {
+    var record1 = result.data;
+    console.log(result.data);
+
+    localStorage.setItem("eventCategory", JSON.stringify(result.data));
+
+    var catDetails = result.data.CategoryDetails;
+    if (catDetails != null && catDetails != undefined) {
+
+      for (index = 0; index < catDetails.length; index++) {
+        var tr = document.createElement("tr");
+        var td1 = document.createElement("td");
+        td1.innerHTML = index + 1;
+        tr.appendChild(td1);
+
+        var td2 = document.createElement("td");
+        td2.innerHTML = catDetails[index].CategoryName;
+        tr.appendChild(td2);
+        catList = catList +  catDetails[index].CategoryName +" ,";
+
+        var td3 = document.createElement("td");
+        td3.innerHTML = catDetails[index].Gender + "-" + catDetails[index].EventType;
+        tr.appendChild(td3);
+
+        var td4 = document.createElement("td");
+        td4.innerHTML = catDetails[index].Fees;
+        tr.appendChild(td4);
+        document.getElementById("eventCategoryDetails").appendChild(tr);
+
+      }
+      document.getElementById("eventCategory").innerHTML = catList;
+
+    }
+  });
+}
+
+
+var btnBooking = document.getElementById("btnBooking");
+btnBooking.addEventListener('click', btnBookingClick, false);
+function btnBookingClick()
+{
+  //e.preventDefault();
+  window.location.href = "eventRegistration.html?id=" + document.getElementById("hfEventID").value;
+};
