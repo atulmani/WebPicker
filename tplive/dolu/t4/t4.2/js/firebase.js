e0b87342e518ff66bfb07a96c434572b70e762e8
atulmani <atulmani@gmail.com>
2022-04-26 01:05:32 +0530

    Update firebase.js

--- tplive/dolu/t4/t4.2/js/firebase.js
+++ tplive/dolu/t4/t4.2/js/firebase.js
@@ -1,13 +1,26 @@
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC8xoYejrqfJflk70HMw877vd3cbUrG3so",
+//   authDomain: "tpliveapp-dev-56bb5.firebaseapp.com",
+//   projectId: "tpliveapp-dev-56bb5",
+//   databaseURL: "https://tpliveapp-dev-56bb5.firebaseio.com",
+//   storageBucket: "tpliveapp-dev-56bb5.appspot.com",
+//   messagingSenderId: "259685787158",
+//   appId: "1:259685787158:web:fd3c4a7bbb3720c3d33737"
+// };
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8xoYejrqfJflk70HMw877vd3cbUrG3so",
-  authDomain: "tpliveapp-dev-56bb5.firebaseapp.com",
-  projectId: "tpliveapp-dev-56bb5",
-  databaseURL: "https://tpliveapp-dev-56bb5.firebaseio.com",
-  storageBucket: "tpliveapp-dev-56bb5.appspot.com",
-  messagingSenderId: "259685787158",
-  appId: "1:259685787158:web:fd3c4a7bbb3720c3d33737"
+  apiKey: "AIzaSyCCwcmd-j8LgBcItu7okMGNt-epPMVx9V0",
+  authDomain: "tplive-uat-f9355.firebaseapp.com",
+  projectId: "tplive-uat-f9355",
+  databaseURL: "https://tplive-uat-f9355.firebaseio.com",
+  storageBucket: "tplive-uat-f9355.appspot.com",
+  messagingSenderId: "843648698798",
+  appId: "1:843648698798:web:cab3c5393bb1645245603f",
+  measurementId: "G-ZMP4Q0QMX4"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
