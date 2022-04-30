3f468ffc82cbbdf61436e4230c1b436804e51c56
atulmani <atulmani@gmail.com>
2022-04-30 21:16:24 +0530

    new version

---
+++ tplive/dolu/t4/t4.3/js/homepage.js
@@ -0,0 +1,100 @@
+
+var loggedinUser = "";
+auth.onAuthStateChanged(firebaseUser => {
+  try {
+    if (firebaseUser) {
+      loggedinUser = firebaseUser;
+      console.log('Logged-in user email id: ' + firebaseUser.email);
+      // userID = firebaseUser.uid;
+      // GetUserRole(firebaseUser);
+      GetProfileData();
+      //document.getElementById('ifSignedIn').innerHTML = 'Hi Sanidhya';
+    } else {
+      loggedinUser = null;
+      console.log('User has been logged out');
+    }
+  } catch (error) {
+    console.log(error.message);
+    //window.location.href = "../index.html";
+  }
+});
+
+function GetProfileData()
+{
+  var userProfile =JSON.parse( localStorage.getItem("userProfile"));
+
+  if(userProfile != undefined && userProfile != "" && userProfile != null )
+  {
+        document.getElementById('ifSignedIn').innerHTML = 'Hi '+ userProfile.UserName;
+        document.getElementById('ifSignedIn').href = "login/profile.html";
+        document.getElementById("UserCity").innerHTML = userProfile.City;
+  }
+
+}
+//
+// function GetProfileDataOld()
+// {
+//
+//     var para1 = {};
+//     para1 = {
+//       userID: loggedinUser.uid
+//     };
+//     console.log(para1);
+//       const ret1 = firebase.functions().httpsCallable("getProfileDetails");
+//       ret1(para1).then((result) => {
+//         var record1 = result.data;
+//         console.log(result.data);
+//         document.getElementById('ifSignedIn').innerHTML = 'Hi '+ result.data.UserName;
+//         document.getElementById('ifSignedIn').href = "login/profile.html";
+//         document.getElementById("UserCity").innerHTML = result.data.City;
+//       });
+//
+// }
+
+// function GetProfileDataOld() {
+//
+//   const snapshot = db.collection('UserList').doc(loggedinUser.uid);
+//   snapshot.get().then(async (doc) => {
+//     if (doc.exists) {
+//       console.log(doc.data());
+//       document.getElementById('ifSignedIn').innerHTML = 'Hi '+ doc.data().UserName;
+//       document.getElementById('ifSignedIn').href = "login/profile.html";
+//       document.getElementById("UserCity").innerHTML = doc.data().City;
+//
+//       // document.getElementById('uploadFile').value = selected_value;
+//     }
+//   });
+// }
+
+
+// var profiletag = document.getElementById('profiletag');
+ function profileDirection(profiletag)
+ {
+  // console.log(loggedinUser.uid);
+  if (loggedinUser === null || loggedinUser === '' )
+  {
+    window.location.href = "./login/index.html";
+  }
+  else{
+    window.location.href = "./login/profile.html";
+  }
+  // auth.onAuthStateChanged(firebaseUser => {
+  //   try {
+  //     if (firebaseUser) {
+  //       loggedinUser = firebaseUser;
+  //       console.log('Logged-in user email id: ' + firebaseUser.email);
+  //       // userID = firebaseUser.uid;
+  //       // GetUserRole(firebaseUser);
+  //
+  //       document.getElementById('ifSignedIn').innerHTML = 'Hi Sanidhya'
+  //     } else {
+  //       loggedinUser = null;
+  //       console.log('User has been logged out');
+  //
+  //     }
+  //   } catch (error) {
+  //     console.log(error.message);
+  //     //window.location.href = "../index.html";
+  //   }
+  // });
+ }
