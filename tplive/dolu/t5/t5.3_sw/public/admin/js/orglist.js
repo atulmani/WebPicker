9d82320998461e05d44cb27f91494c70437bb70c
atulmani <atulmani@gmail.com>
2022-07-19 22:37:23 +0530

    code

--- tplive/dolu/t5/t5.3_sw/public/admin/js/orglist.js
+++
@@ -1,5 +0,0 @@
-function showHideCard(card, cardArrow) {
-  card.classList.toggle("active");
-
-  cardArrow.classList.toggle("active");
-}
--- tplive/dolu/t5/t5.3_sw/public/js/eventDetails.js
+++ tplive/dolu/t5/t5.3_sw/public/js/eventDetails.js
@@ -62,72 +62,115 @@ function GetEventDetails() {
   ret1(para1).then((result) => {
     var record1 = result.data;
     console.log(result.data);
-    var valList ;
+    var valList;
     document.getElementById("eventLogo").src = result.data.EventLogo;
+    if(result.data.ThumbImage1 != undefined && result.data.ThumbImage1 != null && result.data.ThumbImage1 != ""){
+      document.getElementById("thumb1").src = result.data.ThumbImage1;
+    } else {
+      document.getElementById("thumb1").src = result.data.EventLogo;
+    }
     document.getElementById("thumb1").src = result.data.EventLogo;
+
+    if(result.data.ThumbImage2 != undefined && result.data.ThumbImage2 != null && result.data.ThumbImage2 != ""){
+      document.getElementById("thumb2").src = result.data.ThumbImage2;
+    } else {
+      console.log('thumb2 is null');
+      document.getElementById("thumb2").src = "";
+      document.getElementById("thumb2").style.display= "none;" ;
+    }
+
+    if(result.data.ThumbImage3 != undefined && result.data.ThumbImage3 != null && result.data.ThumbImage3 != ""){
+      document.getElementById("thumb3").src = result.data.ThumbImage3;
+    } else {
+      console.log('thumb3 is null');
+      document.getElementById("thumb3").src = "";
+      document.getElementById("thumb3").style.display= "none;" ;
+    }
+    if(result.data.ThumbImage4 != undefined && result.data.ThumbImage4 != null && result.data.ThumbImage4 != ""){
+      document.getElementById("thumb4").src = result.data.ThumbImage4;
+    } else {
+      console.log('thumb4 is null');
+      document.getElementById("thumb4").src = ""
+      document.getElementById("thumb4").style.display= "none;" ;
+    }
+
+    // document.getElementById("thumb1").src = result.data.EventLogo;
+    // document.getElementById("thumb2").src = result.data.EventLogo;
+    // document.getElementById("thumb3").src = result.data.EventLogo;
+    // document.getElementById("thumb4").src = result.data.EventLogo;
     document.getElementById("hfEventID").value = result.data.Eventid;
     document.getElementById("hfOrganizerID").value = result.data.OrganizerID;
     document.getElementById("eventName").innerHTML = result.data.EventName;
     document.getElementById("organisername").innerHTML = result.data.EventOwnerName;
-    if(result.data.EventStartDate != undefined && result.data.EventStartDate != null && result.data.EventStartDate != "")
-    {
+    document.getElementById("announcement").innerHTML = result.data.Announcement;
+
+    if (result.data.EventMode != null && result.data.EventMode != undefined && result.data.EventMode != "") {
+      if (result.data.EventMode.toUpperCase() === "BOOK") {
+        document.getElementById("btn1").innerHTML = "Register";
+        document.getElementById("btn2").innerHTML = "Pay Now";
+      } else if (result.data.EventMode.toUpperCase() === "OPEN") {
+        document.getElementById("btn1").style.display = "none;";
+        document.getElementById("btn2").style.display = "none;";
+      } else if (result.data.EventMode.toUpperCase() === "FIXTURE") {
+        document.getElementById("btn1").innerHTML = "View Draw";
+        document.getElementById("btn2").style.display = "none;";
+      }
+    }
+
+    if (result.data.EventStartDate != undefined && result.data.EventStartDate != null && result.data.EventStartDate != "") {
       var sDate = new Date(result.data.EventStartDate._seconds * 1000);
       document.getElementById("eventstartdate").innerHTML = sDate.toLocaleDateString("en-IN", options);;
     }
-    if(result.data.EventEndDate != undefined && result.data.EventEndDate != null && result.data.EventEndDate != "")
-    {
+    if (result.data.EventEndDate != undefined && result.data.EventEndDate != null && result.data.EventEndDate != "") {
       var eDate = new Date(result.data.EventEndDate._seconds * 1000);
-      document.getElementById("eventenddate").innerHTML = eDate.toLocaleDateString("en-IN", options);;
-    }else
-    {
-      document.getElementById("dateLink").style.display="none;";
+      document.getElementById("eventenddate").innerHTML = " - " + eDate.toLocaleDateString("en-IN", options);;
+    } else {
+      //document.getElementById("dateLink").style.display="none;";
     }
-
-
-
-if (result.data.MinimumFee != null && result.data.MinimumFee != undefined && result.data.MinimumFee != "") {
-  if (result.data.MaximumFee != null && result.data.MaximumFee != undefined && result.data.MaximumFee != "") {
-    if (result.data.MinimumFee != result.data.MaximumFee) {
+    if (result.data.MinimumFee != null && result.data.MinimumFee != undefined && result.data.MinimumFee != "") {
+      if (result.data.MaximumFee != null && result.data.MaximumFee != undefined && result.data.MaximumFee != "") {
+        if (result.data.MinimumFee != result.data.MaximumFee) {
+          // console.log(doc.MinimumFee);
+          // h32.innerHTML = doc.MinimumFee.toLocaleString('en-IN', curFormat) + " - " + doc.MaximumFee.toLocaleString('en-IN', curFormat);
+          document.getElementById("eventprice").innerHTML = Number(result.data.MinimumFee).toLocaleString('en-IN', curFormat) +
+            " - " + result.data.MaximumFee.toLocaleString('en-IN', curFormat);
+        } else {
+          // console.log(doc.MinimumFee);
+          document.getElementById("eventprice").innerHTML = Number(result.data.MinimumFee).toLocaleString('en-IN', curFormat);
+        }
+      } else {
         // console.log(doc.MinimumFee);
-      // h32.innerHTML = doc.MinimumFee.toLocaleString('en-IN', curFormat) + " - " + doc.MaximumFee.toLocaleString('en-IN', curFormat);
-      document.getElementById("eventprice").innerHTML = Number(result.data.MinimumFee).toLocaleString('en-IN', curFormat)
-      + " - " + result.data.MaximumFee.toLocaleString('en-IN', curFormat);
+        document.getElementById("eventprice").innerHTML = Number(result.data.MinimumFee).toLocaleString('en-IN', curFormat);
+      }
     } else {
       // console.log(doc.MinimumFee);
-      document.getElementById("eventprice").innerHTML = Number(result.data.MinimumFee).toLocaleString('en-IN', curFormat);
+      document.getElementById("eventprice").innerHTML = "-";
     }
-  } else {
-    // console.log(doc.MinimumFee);
-    document.getElementById("eventprice").innerHTML = Number(result.data.MinimumFee).toLocaleString('en-IN', curFormat);
-  }
-} else {
-  // console.log(doc.MinimumFee);
-  document.getElementById("eventprice").innerHTML = "-";
-}
-document.getElementById("eventVenue").innerHTML = result.data.EventVenue;
+    document.getElementById("eventVenue").innerHTML = result.data.EventVenue;
 
-if(result.data.WithdrawalEndDate != undefined && result.data.WithdrawalEndDate != null && result.data.WithdrawalEndDate != "")
-{
-  var wDate = new Date(result.data.WithdrawalEndDate._seconds * 1000);
-  document.getElementById("withdrawalDate").innerHTML = wDate.toLocaleDateString("en-IN", options);
-}
-// document.getElementById("eventstatus").innerHTML = result.data.EventStatus;
+    if (result.data.WithdrawalEndDate != undefined && result.data.WithdrawalEndDate != null && result.data.WithdrawalEndDate != "") {
+      var wDate = new Date(result.data.WithdrawalEndDate._seconds * 1000);
+      document.getElementById("withdrawalDate").innerHTML = wDate.toLocaleDateString("en-IN", options);
+    }
+    // document.getElementById("eventstatus").innerHTML = result.data.EventStatus;
 
-document.getElementById("organiserEmail").innerHTML = result.data.EventOwnerEmail;
-document.getElementById("organiserEmail").href = "mailto: " + result.data.EventOwnerEmail;
-document.getElementById("organiserPhone").innerHTML = result.data.EventOwnerPhone;
-document.getElementById("organiserPhone").href="tel: +" + result.data.EventOwnerPhone;
+    document.getElementById("organiserEmail").innerHTML = result.data.EventOwnerEmail;
+    document.getElementById("organiserEmail").href = "mailto: " + result.data.EventOwnerEmail;
+    document.getElementById("organiserPhone").innerHTML = result.data.EventOwnerPhone;
+    document.getElementById("organiserPhone").href = "tel: +" + result.data.EventOwnerPhone;
 
+    // document.getElementById("eventDetails").innerHTML = result.data.EventDetails;
+    if(result.data.RulesAndRegulations != undefined && result.data.RulesAndRegulations != null){
 
-    //
-    // valList = result.data.CategoryDetails;
-    // console.log(valList);
-    // document.getElementById("existingEventCategory").innerHTML = "";
-    // if (valList != null && valList != undefined) {
-    //   for (index = 0; index < valList.length; index++) {
-    //     renderCategory(valList[index], index);
-    //   }
-    // }
+      document.getElementById("RulesAndRegulations").innerHTML = result.data.RulesAndRegulations.replaceAll(";", "<br>");
+    }
+    // console.log(result.data.RulesAndRegulations.replace(";", "<br>"));
+    valList = result.data.CategoryDetails;
+    if (valList != null && valList != undefined) {
+      for (index = 0; index < valList.length; index++) {
+        renderCategory(valList[index], index);
+      }
+    }
   });
 }
 
@@ -145,105 +188,58 @@ function renderCategory(obj, index) {
     month: 'short',
     day: 'numeric'
   };
+  /*
+  <tr>
+    <td>1</td>
+    <td>Boys Double Under11</td>
+    <td>M</td>
+    <td>600</td>
+  </tr>
+  */
+  var tr = document.createElement("tr");
 
-  var div1 = document.createElement("div");
-  div1.setAttribute("class", "form-group col-md-12");
+  var td1 = document.createElement("td");
+  td1.innerHTML = index + 1;
+  tr.appendChild(td1);
+  var td2 = document.createElement("td");
+  td2.innerHTML = obj.CategoryName;
+  tr.appendChild(td2);
 
-  var div2 = document.createElement("div");
-  div2.setAttribute("class", "item");
+  var td3 = document.createElement("td");
+  td3.innerHTML = obj.Gender + " - " + obj.EventType;
+  tr.appendChild(td3);
 
-  var div3 = document.createElement("div");
-  div3.setAttribute("class", "post-slide");
-
-  var div4 = document.createElement("div");
-  div4.setAttribute("class", "post-content");
-
-  var h1 = document.createElement("h3");
-  h1.setAttribute("class", "post-title");
-  h1.setAttribute("style", "font-weight:bold");
-  h1.innerHTML = obj.CategoryName;
-
-  div4.appendChild(h1);
-
-  var p1 = document.createElement("p");
-  p1.setAttribute("class", "description");
-
-  var span1 = document.createElement("span");
-  span1.setAttribute("style", "font-weight:bold");
-  span1.innerHTML = obj.Gender + " - " + obj.EventType;
-
-  p1.appendChild(span1);
-  var br1 = document.createElement("br");
-  p1.appendChild(br1);
-
+  var td4 = document.createElement("td");
+  td4.innerHTML = obj.Fees.toLocaleString('en-IN', curFormat);
+  tr.appendChild(td4);
   // var refdate = new Date(obj.ReferenceDate.seconds * 1000);
   var refdate = new Date(obj.ReferenceDate._seconds * 1000);
-  var span2 = document.createElement("span");
-  span2.setAttribute("style", "letter-spacing:1px;");
-  span2.innerHTML = "Born " + obj.DateRefType + " : " + refdate.toLocaleDateString("en-US", options);
+  //  span2.innerHTML = "Born " + obj.DateRefType + " : " + refdate.toLocaleDateString("en-US", options);
 
-  p1.appendChild(span2);
-  var br2 = document.createElement("br");
-  p1.appendChild(br2);
 
-  var span3 = document.createElement("span");
-  span3.setAttribute("style", "letter-spacing:1px;");
-  span3.innerHTML = "Max Team Size :" + obj.MaxTeamSize;
-
-  p1.appendChild(span3);
-  var br3 = document.createElement("br");
-  p1.appendChild(br3);
-
-  var span4 = document.createElement("span");
-  span4.setAttribute("style", "letter-spacing:1px;");
-  span4.innerHTML = "Fee :" + obj.Fees.toLocaleString('en-IN', curFormat);
-
-  p1.appendChild(span4);
-  var br4 = document.createElement("br");
-  p1.appendChild(br4);
-
-  var i1 = document.createElement("i");
-  i1.setAttribute("onclick", "EditCategoryDetails(" + index + ");");
-  i1.setAttribute("class", "far fa-edit address-edit-icon");
-  i1.setAttribute("style", "padding: 0 5px 0 5px;");
-  p1.appendChild(i1);
-
-  var i2 = document.createElement("i");
-  i2.setAttribute("onclick", "DeleteCategoryDetails(" + index + ");");
-  i2.setAttribute("class", "far fa-trash address-edit-icon");
-  i2.setAttribute("style", "padding: 0 5px 0 5px;");
-  p1.appendChild(i2);
-  //
-  // var br4 = document.createElement("br");
-  // p1.appendChild(br4);
-  div4.appendChild(p1);
-  div3.appendChild(div4);
-  div2.appendChild(div3);
-  div1.appendChild(div2);
-  document.getElementById("existingEventCategory").appendChild(div1);
+  // var span3 = document.createElement("span");
+  // span3.setAttribute("style", "letter-spacing:1px;");
+  // span3.innerHTML = "Max Team Size :" + obj.MaxTeamSize;
+  document.getElementById("eventCategoryDetails").appendChild(tr);
 }
 
 // js for product gallery
 
-    var ProductImg = document.getElementById("eventLogo");
-    var SmallImg = document.getElementsByClassName("small-img");
+var ProductImg = document.getElementById("eventLogo");
+var SmallImg = document.getElementsByClassName("small-img");
 
-      SmallImg[0].onclick = function()
-      {
-        ProductImg.src = SmallImg[0].src;
-      }
-      SmallImg[1].onclick = function()
-      {
-        ProductImg.src = SmallImg[1].src;
-      }
-      SmallImg[2].onclick = function()
-      {
-        ProductImg.src = SmallImg[2].src;
-      }
-      SmallImg[3].onclick = function()
-      {
-        ProductImg.src = SmallImg[3].src;
-      }
+SmallImg[0].onclick = function() {
+  ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function() {
+  ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function() {
+  ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function() {
+  ProductImg.src = SmallImg[3].src;
+}
 
 
 // js for toggle form
