47d489c056b6a744c1f52ecf95d79d6dfac03eb8
atulmani <atulmani@gmail.com>
2022-08-06 10:13:27 +0530

    addproperty

--- Propdial/dolu/t1/t1.2/js/addproperty.js
+++ Propdial/dolu/t1/t1.2/js/addproperty.js
@@ -3,40 +3,61 @@ function addPropertyMenu(menuList) {
 
   var menuIndicator = document.getElementById('menuIndicator');
   var Basic = document.getElementById('Basic');
-  var Source = document.getElementById('Source');
-  var Category = document.getElementById('Category');
-  var Flag = document.getElementById('Flag');
+  var Details = document.getElementById('Details');
+  var Occupancy = document.getElementById('Occupancy');
+  var History = document.getElementById('History');
+
+  var section1 = document.getElementById("section1");
+  var section2 = document.getElementById("section2");
+  var section3 = document.getElementById("section3");
+  var section4 = document.getElementById("section4");
 
   Basic.classList.remove('active');
-  Source.classList.remove('active');
-  Category.classList.remove('active');
-  Flag.classList.remove('active');
+  Details.classList.remove('active');
+  Occupancy.classList.remove('active');
+  History.classList.remove('active');
 
   if (menuList === 'Basic') {
     menuIndicator.style.width = '25%';
     menuIndicator.style.borderRadius = '0 55px 55px 0';
     Basic.classList.add('active');
+    section1.style.transform = 'translateX(0%)';
+    section2.style.transform = 'translateX(0%)';
+    section3.style.transform = 'translateX(0%)';
+    section4.style.transform = 'translateX(0%)';
   }
-  else if (menuList === 'Source') {
+  else if (menuList === 'Details') {
     menuIndicator.style.width = '50%';
     menuIndicator.style.borderRadius = '0 55px 55px 0';
     Basic.classList.add('active');
-    Source.classList.add('active');
+    Details.classList.add('active');
+    section1.style.transform = 'translateX(-100%)';
+    section2.style.transform = 'translateX(-100%)';
+    section3.style.transform = 'translateX(-100%)';
+    section4.style.transform = 'translateX(-100%)';
   }
-  else if (menuList === 'Category') {
+  else if (menuList === 'Occupancy') {
     menuIndicator.style.width = '75%';
     menuIndicator.style.borderRadius = '0 55px 55px 0';
     Basic.classList.add('active');
-    Source.classList.add('active');
-    Category.classList.add('active');
+    Details.classList.add('active');
+    Occupancy.classList.add('active');
+    section1.style.transform = 'translateX(-200%)';
+    section2.style.transform = 'translateX(-200%)';
+    section3.style.transform = 'translateX(-200%)';
+    section4.style.transform = 'translateX(-200%)';
   }
-  else if (menuList === 'Flag') {
+  else if (menuList === 'History') {
     menuIndicator.style.width = '100%';
     menuIndicator.style.borderRadius = '0';
     Basic.classList.add('active');
-    Source.classList.add('active');
-    Category.classList.add('active');
-    Flag.classList.add('active');
+    Details.classList.add('active');
+    Occupancy.classList.add('active');
+    History.classList.add('active');
+    section1.style.transform = 'translateX(-300%)';
+    section2.style.transform = 'translateX(-300%)';
+    section3.style.transform = 'translateX(-300%)';
+    section4.style.transform = 'translateX(-300%)';
   }
 
 }
