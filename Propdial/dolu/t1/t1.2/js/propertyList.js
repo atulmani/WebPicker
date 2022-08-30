246536288254aa787715dbca8d31d5031d79fe6d
atulmani <atulmani@gmail.com>
2022-08-30 23:27:36 +0530

    propdail

--- Propdial/dolu/t1/t1.2/js/propertyList.js
+++ Propdial/dolu/t1/t1.2/js/propertyList.js
@@ -1,13 +1,23 @@
 function openFilterAdvancePopup() {
-    var advanceSearchPopup = document.getElementById('advanceSearchPopup');
+    var advanceSearchBtn = document.getElementById('advanceSearchBtn');
+    var advanceFilterPopUp = document.getElementById('advanceFilterPopUp');
+    var advanceFilterPopUpContent = document.getElementById('advanceFilterPopUpContent');
+    var advanceFilterPopUpSearchBtn = document.getElementById('advanceFilterPopUpSearchBtn');
 
-    advanceSearchPopup.style.opacity = '1';
-    advanceSearchPopup.style.pointerEvents = 'all';
+    advanceSearchBtn.style.display = 'none';
+    advanceFilterPopUp.classList.add('open');
+    advanceFilterPopUpContent.style.opacity = '1';
+    advanceFilterPopUpSearchBtn.style.opacity = '1';
 }
 
 function closeFilterAdvancePopup() {
-    var advanceSearchPopup = document.getElementById('advanceSearchPopup');
+    var advanceSearchBtn = document.getElementById('advanceSearchBtn');
+    var advanceFilterPopUp = document.getElementById('advanceFilterPopUp');
+    var advanceFilterPopUpContent = document.getElementById('advanceFilterPopUpContent');
+    var advanceFilterPopUpSearchBtn = document.getElementById('advanceFilterPopUpSearchBtn');
 
-    advanceSearchPopup.style.opacity = '0';
-    advanceSearchPopup.style.pointerEvents = 'none';
+    advanceSearchBtn.style.display = 'block';
+    advanceFilterPopUp.classList.remove('open');
+    advanceFilterPopUpContent.style.opacity = '0';
+    advanceFilterPopUpSearchBtn.style.opacity = '0';
 }
