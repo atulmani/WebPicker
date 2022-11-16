eefe71edde804ea7d1af665e703a4e3c75eac59e
atulmani <atulmani@gmail.com>
2022-11-16 08:49:58 +0530

    Update regCategory.js

--- tplive/dolu/t5/t5.3_sw/public/js/regCategory.js
+++ tplive/dolu/t5/t5.3_sw/public/js/regCategory.js
@@ -241,3 +241,95 @@ function getEventDetails() {
 
     });
 }
+
+
+function closePartnerSelection(doubleDiv) {
+    doubleDiv.classList.remove('show');
+    doubleDiv.style.overflow = 'hidden';
+}
+
+function moveToRegCategory() {
+    window.location.href = "regCategory.html";
+}
+function openPartnerSelection(doubleDiv) {
+
+    if (doubleDiv.classList.contains('show')) {
+        doubleDiv.classList.remove('show');
+        doubleDiv.style.overflow = 'hidden';
+    } else {
+        doubleDiv.classList.add('show');
+
+        setTimeout(() => {
+            doubleDiv.style.overflow = 'visible';
+        }, 1000)
+    }
+}
+function selectCategory(category, entryFees) {
+    category.classList.toggle('active');
+
+    var feesInNumber = Number(entryFees.innerHTML);
+
+    var totalPrice = document.getElementById('totalPrice');
+    var noOfCategories = document.getElementById('noOfCategories');
+
+    var totalPriceInNumber = Number(totalPrice.innerHTML);
+    var totalNoOfCategories = Number(noOfCategories.innerHTML);
+
+    if (category.classList.contains('active')) {
+        totalPrice.innerHTML = totalPriceInNumber + feesInNumber;
+        noOfCategories.innerHTML = totalNoOfCategories + 1;
+    } else {
+        totalPrice.innerHTML = totalPriceInNumber - feesInNumber;
+        noOfCategories.innerHTML = totalNoOfCategories - 1;
+    }
+
+    var checkOutDiv = document.getElementById('checkOutDiv');
+    console.log(totalPriceInNumber);
+
+    totalPriceInNumber = Number(totalPrice.innerHTML);
+
+    if (totalPriceInNumber <= 0) {
+        checkOutDiv.style.opacity = '0';
+        checkOutDiv.style.pointerEvents = 'none';
+        console.log('Inside else');
+    } else {
+        checkOutDiv.style.opacity = '1';
+        checkOutDiv.style.pointerEvents = 'all';
+        console.log('Inside <=0');
+    }
+
+}
+function regProfileToThirdSlide() {
+    var regProfileFirstSlide = document.getElementById('regProfileFirstSlide');
+    var regProfileSecondSlide = document.getElementById('regProfileSecondSlide');
+    var regProfileThirdSlide = document.getElementById('regProfileThirdSlide');
+
+    regProfileFirstSlide.style.transform = 'translateX(-200%)';
+    regProfileSecondSlide.style.transform = 'translateX(-200%)';
+    regProfileThirdSlide.style.transform = 'translateX(-200%)';
+
+    document.getElementById('firstFormDot').classList.remove('active');
+    document.getElementById('SecondFormDot').classList.add('active');
+}
+function regProfileToSecondSlide() {
+    var regProfileFirstSlide = document.getElementById('regProfileFirstSlide');
+    var regProfileSecondSlide = document.getElementById('regProfileSecondSlide');
+    var regProfileThirdSlide = document.getElementById('regProfileThirdSlide');
+
+    regProfileFirstSlide.style.transform = 'translateX(-100%)';
+    regProfileSecondSlide.style.transform = 'translateX(-100%)';
+    regProfileThirdSlide.style.transform = 'translateX(-100%)';
+
+    document.getElementById('firstFormDot').classList.add('active');
+    document.getElementById('SecondFormDot').classList.remove('active');
+}
+function regProfileToFirstSlide() {
+    var regProfileFirstSlide = document.getElementById('regProfileFirstSlide');
+    var regProfileSecondSlide = document.getElementById('regProfileSecondSlide');
+    var regProfileThirdSlide = document.getElementById('regProfileThirdSlide');
+
+    regProfileFirstSlide.style.transform = 'translateX(0%)';
+    regProfileSecondSlide.style.transform = 'translateX(0%)';
+    regProfileThirdSlide.style.transform = 'translateX(0%)';
+}
+
