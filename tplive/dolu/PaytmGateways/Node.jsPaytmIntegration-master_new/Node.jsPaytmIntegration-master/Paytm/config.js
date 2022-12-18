cdc5c45545f4992fd4f5a5dc096fd2c0c12b5601
atulmani <atulmani@gmail.com>
2022-12-18 14:43:25 +0530

    PaytmIntegration

---
+++ tplive/dolu/PaytmGateways/Node.jsPaytmIntegration-master_new/Node.jsPaytmIntegration-master/Paytm/config.js
@@ -0,0 +1,13 @@
+// var PaytmConfig = {
+//   mid: "Tourna05657364781618",
+//   key: "3gkJ!L1XwzDE9&Zh",
+//   website: "WEBSTAGING",
+// };
+
+var PaytmConfig = {
+  mid: "DIY12386817555501617",
+  key: "bKMfNxPPf_QdZppa",
+  website: "WEBSTAGING",
+};
+
+module.exports.PaytmConfig = PaytmConfig;
