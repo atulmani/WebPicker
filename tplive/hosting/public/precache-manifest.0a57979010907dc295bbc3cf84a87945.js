655f6d6acefa08b1d50cccb398f34ae8181dfe1c
atulmani <atulmani@gmail.com>
2023-03-28 23:48:06 +0530

    rozarPay intergration

---
+++ tplive/hosting/public/precache-manifest.0a57979010907dc295bbc3cf84a87945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cb86d9891a728cd6384",
+    "url": "/static/css/main.e2f8e794.chunk.css"
+  },
+  {
+    "revision": "0cb86d9891a728cd6384",
+    "url": "/static/js/main.0cb86d98.chunk.js"
+  },
+  {
+    "revision": "c1bcd1d300ed4e5074ce",
+    "url": "/static/js/1.c1bcd1d3.chunk.js"
+  },
+  {
+    "revision": "9f369409ce558a28177d",
+    "url": "/static/css/2.4072e7ff.chunk.css"
+  },
+  {
+    "revision": "9f369409ce558a28177d",
+    "url": "/static/js/2.9f369409.chunk.js"
+  },
+  {
+    "revision": "e27947866dd61858c03c",
+    "url": "/static/js/runtime~main.e2794786.js"
+  },
+  {
+    "revision": "1693159b3c419f6db0b2cb6a77a4d47f",
+    "url": "/index.html"
+  }
+];
